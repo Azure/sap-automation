c9644a0f55c47a104c5ca2af5095993c61d2f0ec
Nadeen Noaman <95418928+nnoaman@users.noreply.github.com>
2021-12-30 12:32:17 +0100

    Update setup_ado.sh

--- deploy/scripts/setup_ado.sh
+++ deploy/scripts/setup_ado.sh
@@ -4,7 +4,7 @@
 
   mkdir -p ~/agent; cd $_
 
-  wget https://vstsagentpackage.azureedge.net/agent/2.196.1/vsts-agent-linux-x64-2.196.1.tar.gz agent.tar.gz
+  wget https://vstsagentpackage.azureedge.net/agent/2.196.1/vsts-agent-linux-x64-2.196.1.tar.gz -O agent.tar.gz
 
   tar zxvf agent.tar.gz
 
