8955a72f129c11426e230339d6ba73ca3b2072fd
William Sheehan <wsheehan@microsoft.com>
2022-01-24 19:48:24 -0800

    az pipelines query fixes

--- deploy/scripts/prepare_region.sh
+++ deploy/scripts/prepare_region.sh
@@ -432,8 +432,8 @@ if [ 2 == $step ]; then
 
     terraform_module_directory="${DEPLOYMENT_REPO_PATH}"/deploy/terraform/bootstrap/sap_library/
     export TF_VAR_cmdb_connection_string=$(terraform -chdir="${terraform_module_directory}" output cmdb_connection_string | tr -d \")
+    specificGroupId=$(az pipelines variable-group list --query "[?name=='SAP-deployment-variables-specific'].id | [0]")
     az pipelines variable-group variable create --group-id $specificGroupId --name TF_VAR_cmdb_connection_string --value $TF_VAR_cmdb_connection_string
-    save_config_var "specificGroupId" "${deployer_config_information}"
     
     cd "${curdir}" || exit
     export step=3
@@ -471,7 +471,7 @@ if [ 3 == $step ]; then
     fi
     allParams=$(printf " --parameterfile %s --storageaccountname %s --type sap_deployer %s %s " "${deployer_file_parametername}" "${REMOTE_STATE_SA}" "${approveparam}" "${ado_flag}" )
 
-    load_config_vars "${deployer_config_information}" "specificGroupId"
+    specificGroupId=$(az pipelines variable-group list --query "[?name=='SAP-deployment-variables-specific'].id | [0]")
     export TF_VAR_cmdb_connection_string=$(az pipelines variable-group variable list --group-id $specificGroupId --query "TF_VAR_cmdb_connection_string.value" | tr -d '"')
     
     "${DEPLOYMENT_REPO_PATH}"/deploy/scripts/installer.sh $allParams
