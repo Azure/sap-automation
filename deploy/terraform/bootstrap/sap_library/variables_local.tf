234b10e7d14a85626f44e9facbb4378b908a1e3f
Kimmo Forss <kimforss@microsoft.com>
2022-02-04 10:34:14 +0000

    Misc fixes

--- deploy/terraform/bootstrap/sap_library/variables_local.tf
+++ deploy/terraform/bootstrap/sap_library/variables_local.tf
@@ -5,10 +5,10 @@ locals {
   // If custom names are used for deployer, providing resource_group_name and msi_name will override the naming convention
   deployer_rg_name = try(local.deployer.resource_group_name, format("%s%s", local.deployer_prefix, module.sap_namegenerator.naming.resource_suffixes.deployer_rg))
   // Retrieve the arm_id of deployer's Key Vault from deployer's terraform.tfstate
-  spn_key_vault_arm_id = coalesce(local.key_vault.kv_spn_id, try(data.terraform_remote_state.deployer[0].outputs.deployer_kv_user_arm_id, ""))
+  spn_key_vault_arm_id = try(coalesce(local.key_vault.kv_spn_id, try(data.terraform_remote_state.deployer[0].outputs.deployer_kv_user_arm_id, "")), "")
   
 spn =  {
-    subscription_id = data.azurerm_key_vault_secret.subscription_id.value,
+    subscription_id = var.use_deployer ? data.azurerm_key_vault_secret.subscription_id[0].value : null,
     client_id       = var.use_deployer ? data.azurerm_key_vault_secret.client_id[0].value : null,
     client_secret   = var.use_deployer ? data.azurerm_key_vault_secret.client_secret[0].value : null,
     tenant_id       = var.use_deployer ? data.azurerm_key_vault_secret.tenant_id[0].value : null
@@ -21,7 +21,7 @@ spn =  {
   }
 
   account = {
-    subscription_id = data.azurerm_key_vault_secret.subscription_id.value,
+    subscription_id = var.use_deployer ? data.azurerm_key_vault_secret.subscription_id[0].value : null,
     tenant_id       = data.azurerm_client_config.current.tenant_id,
     object_id       = data.azurerm_client_config.current.object_id
   }
