de6332eda8aa12738784134f04c455e76a946df0
Kimmo Forss <kimmo.forss@microsoft.com>
2022-04-04 16:53:08 +0300

    correct if

--- deploy/terraform/run/sap_system/variables_local.tf
+++ deploy/terraform/run/sap_system/variables_local.tf
@@ -144,5 +144,5 @@ locals {
     object_id       = data.azurerm_client_config.current.object_id
   }
 
-  custom_names = length(var.name_overrride_file) > 0 ? jsondecode(file(format("%s/%s", path.cwd, var.name_overrride_file))) : {}
+  custom_names = length(var.name_overrride_file) > 0 ? jsondecode(file(format("%s/%s", path.cwd, var.name_overrride_file))) : null
 }
