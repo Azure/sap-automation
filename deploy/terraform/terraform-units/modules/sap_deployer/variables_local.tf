86c08fceae69809deea1bbffa43ae589b7d526ed
Kimmo Forss <kimmo.forss@microsoft.com>
2022-04-07 17:35:57 +0300

    Add a regex for secrets

--- deploy/terraform/terraform-units/modules/sap_deployer/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/variables_local.tf
@@ -188,10 +188,10 @@ locals {
   pwd_exist      = try(length(local.input_password_secret_name) > 0, false)
   username_exist = try(length(local.input_username_secret_name) > 0, false)
 
-  ppk_secret_name      = local.key_exist ? local.input_private_key_secret_name : format("%s-sshkey", length(local.prefix) > 0 ? local.prefix : "DEP00")
-  pk_secret_name       = local.key_exist ? local.input_public_key_secret_name : format("%s-sshkey-pub", length(local.prefix) > 0 ? local.prefix : "DEP00")
-  pwd_secret_name      = local.pwd_exist ? local.input_password_secret_name : format("%s-password", length(local.prefix) > 0 ? local.prefix : "DEP00")
-  username_secret_name = local.username_exist ? local.input_username_secret_name : format("%s-username", length(local.prefix) > 0 ? local.prefix : "DEP00")
+  ppk_secret_name      = local.key_exist ? local.input_private_key_secret_name : format("%s-sshkey", replace(length(local.prefix) > 0 ? local.prefix : "DEP00"), "/[^A-Za-z0-9]/", "")
+  pk_secret_name       = local.key_exist ? local.input_public_key_secret_name : format("%s-sshkey-pub", replace(length(local.prefix) > 0 ? local.prefix : "DEP00"), "/[^A-Za-z0-9]/", "")
+  pwd_secret_name      = local.pwd_exist ? local.input_password_secret_name : format("%s-password", replace(length(local.prefix) > 0 ? local.prefix : "DEP00"), "/[^A-Za-z0-9]/", "")
+  username_secret_name = local.username_exist ? local.input_username_secret_name : format("%s-username", replace(length(local.prefix) > 0 ? local.prefix : "DEP00"), "/[^A-Za-z0-9]/", "")
 
   // Extract information from the specified key vault arm ids
   user_kv_name    = local.user_kv_exist ? split("/", local.user_key_vault_id)[8] : local.keyvault_names.user_access
