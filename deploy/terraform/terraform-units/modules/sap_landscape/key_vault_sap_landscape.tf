76fcfe09c950c122e0a2319f89f10678912e2ecb
Kimmo Forss <kimmo.forss@microsoft.com>
2022-05-30 11:18:09 +0300

    Put IP back

--- deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
@@ -32,7 +32,10 @@ resource "azurerm_key_vault" "kv_user" {
           length(local.deployer_public_ip_address) > 0 ? local.deployer_public_ip_address : "",
           length(var.Agent_IP) > 0 ? var.Agent_IP : ""
         ]
-      )) : ( [""]
+      )) : (
+      [
+        length(var.Agent_IP) > 0 ? var.Agent_IP : "" 
+      ]
     )
     virtual_network_subnet_ids = [
       local.deployer_subnet_management_id
@@ -303,6 +306,7 @@ resource "azurerm_private_endpoint" "kv_user" {
       azurerm_subnet.app[0].id
     )
   )
+
 
   private_service_connection {
     name = format("%s%s%s",
