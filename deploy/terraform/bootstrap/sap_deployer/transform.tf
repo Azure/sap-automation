837845aca67309836686694e932446e8ae162400
Will Sheehan <willksheehan@gmail.com>
2022-06-23 12:20:32 -0700

    Web app integration (#207)

--- deploy/terraform/bootstrap/sap_deployer/transform.tf
+++ deploy/terraform/bootstrap/sap_deployer/transform.tf
@@ -118,6 +118,10 @@ locals {
           arm_id = var.management_bastion_subnet_arm_id
           prefix = var.management_bastion_subnet_address_prefix
         }
+        subnet_cmdb = {
+          arm_id = var.cmdb_subnet_arm_id
+          prefix = var.cmdb_subnet_address_prefix
+        }
       }
     }
   }
