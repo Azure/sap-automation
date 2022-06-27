b5ce66ac839850d885fb2199624d5f0c51bb96a0
Kimmo Forss <kimforss@microsoft.com>
2022-06-27 11:18:17 +0300

    Load balancer IP update

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/variables_local.tf
@@ -526,7 +526,7 @@ locals {
     false
   )
 
-  winha_ips = [
+  winha_ips = upper(local.scs_ostype) == "WINDOWS" ? [
     {
       name = format("%s%s%s%s",
         var.naming.resource_prefixes.scs_clst_feip,
@@ -544,7 +544,7 @@ locals {
       private_ip_address = length(try(local.scs_lb_ips[2], "")) > 0 ? (
         local.scs_lb_ips[2]) : (
         var.application.use_DHCP ? (
-        null) : (cidrhost(local.application_subnet_prefix, 2 + local.ip_offsets.scs_lb))
+        null) : (cidrhost(data.azurerm_subnet.subnet_sap_app[0].address_prefixes[0], 2 + local.ip_offsets.scs_lb))
       )
       private_ip_address_allocation = length(try(local.scs_lb_ips[2], "")) > 0 ? "Static" : "Dynamic"
 
@@ -566,11 +566,11 @@ locals {
       private_ip_address = length(try(local.scs_lb_ips[3], "")) > 0 ? (
         local.scs_lb_ips[3]) : (
         var.application.use_DHCP ? (
-        null) : (cidrhost(local.application_subnet_prefix, 3 + local.ip_offsets.scs_lb))
+        null) : (cidrhost(data.azurerm_subnet.subnet_sap_app[0].address_prefixes[0], 3 + local.ip_offsets.scs_lb))
       )
       private_ip_address_allocation = length(try(local.scs_lb_ips[3], "")) > 0 ? "Static" : "Dynamic"
     }
-  ]
+  ] : []
 
   std_ips = [
     {
