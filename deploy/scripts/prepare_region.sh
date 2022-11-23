ed38ddbb2501b096128ae8e8ecc1eccccb8441bb
hdamecharla <hdamecharla@microsoft.com>
2022-11-23 13:59:11 +0530

    encase the variable in double quotes

--- deploy/scripts/prepare_region.sh
+++ deploy/scripts/prepare_region.sh
@@ -133,7 +133,7 @@ if [ 0 != $return_code ]; then
     exit $return_code
 fi
 # Check that webapp exports are defined, if deploying webapp
-if [ $TF_VAR_use_webapp == "true" ]; then
+if [ "$TF_VAR_use_webapp" == "true" ]; then
     validate_webapp_exports
     return_code=$?
     if [ 0 != $return_code ]; then
