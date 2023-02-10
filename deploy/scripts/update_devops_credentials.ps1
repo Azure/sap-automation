2474bb07e43f865dabf3eb454b3b433544ce0ced
Morgan Deegan <morgan@unicorn.town>
2023-02-10 07:28:10 -0800

    Cleanup and logging for script

--- deploy/scripts/update_devops_credentials.ps1
+++ deploy/scripts/update_devops_credentials.ps1
@@ -1,131 +1,182 @@
-
-$Organization = $Env:ADO_ORGANIZATION
-
-$Project = $Env:ADO_PROJECT
-
-if ($Organization.Length -eq 0) {
+#----------------------
+# Set $Organization
+#   Input     or    $Env:ADO_ORGANIZATION
+#----------------------
+if ($Env:ADO_ORGANIZATION.Length -eq 0) {
   Write-Host "Organization is not set"
   $Organization = Read-Host "Enter your ADO organization URL"
 }
 else {
-  Write-Host "Using Organization: $Organization"
+  $Organization = $Env:ADO_ORGANIZATION
 }
+Write-Host "Organization:                   $Organization"
+#----------------------
 
-if ($Project.Length -eq 0) {
+#----------------------
+# Set $Project
+#   Input     or    $Env:ADO_PROJECT
+#----------------------
+if ($Env:ADO_PROJECT.Length -eq 0) {
   Write-Host "Project is not set"
-  $Organization = Read-Host "Enter your ADO project name"
+  $Project = Read-Host "Enter your ADO organization URL"
 }
 else {
-  Write-Host "Using Project: $Project"
+  $Project = $Env:ADO_PROJECT
 }
+Write-Host "Project:                        $Project"
+#----------------------
 
+#----------------------
+# Set $SDAF_CONTROL_PLANE_CODE
+#   Default     or    $Env:SDAF_CONTROL_PLANE_CODE
+#----------------------
 if ($Env:SDAF_CONTROL_PLANE_CODE.Length -ne 0) {
- $SDAF_CONTROL_PLANE_CODE = $Env:SDAF_CONTROL_PLANE_CODE
+  $SDAF_CONTROL_PLANE_CODE = $Env:SDAF_CONTROL_PLANE_CODE
 }
-else
-{
-  $SDAF_CONTROL_PLANE_CODE = "MGMT"
-}
+else { $SDAF_CONTROL_PLANE_CODE = "MGMT" }
+Write-Host "SDAF_CONTROL_PLANE_CODE:        $SDAF_CONTROL_PLANE_CODE"
+#----------------------
 
+#----------------------
+# Set $SDAF_WORKLOAD_ZONE_CODE
+#   Default     or    $Env:SDAF_WORKLOAD_ZONE_CODE
+#----------------------
 if ($Env:SDAF_WORKLOAD_ZONE_CODE.Length -ne 0) {
- $SDAF_WORKLOAD_ZONE_CODE = $Env:SDAF_WORKLOAD_ZONE_CODE
+  $SDAF_WORKLOAD_ZONE_CODE = $Env:SDAF_WORKLOAD_ZONE_CODE
 }
-else
-{
-  $SDAF_WORKLOAD_ZONE_CODE = "DEV"
-}
-
+else { $SDAF_WORKLOAD_ZONE_CODE = "DEV" }
+Write-Host "SDAF_WORKLOAD_ZONE_CODE:        $SDAF_WORKLOAD_ZONE_CODE"
+#----------------------
 
 $MgmtPrefix = "SDAF-"+$SDAF_CONTROL_PLANE_CODE
 $DEVPrefix = "SDAF-"+$SDAF_WORKLOAD_ZONE_CODE
-$Name = $MgmtPrefix + "-configuration-app"
 
-if ($Env:SDAF_APP_NAME.Length -ne 0) {
-  $Name = $Env:SDAF_APP_NAME
+#----------------------
+# Set $Name
+#   Default     or    $Env:SDAF_APP_NAME
+#----------------------
+if ($Env:SDAF_APP_NAME.Length -eq 0) {
+  $Name = $MgmtPrefix + "-configuration-app"
 }
+else { $Name = $Env:SDAF_APP_NAME }
+Write-Host "Name:                           $Name"
+#----------------------
 
-$ControlPlaneSubscriptionID = $Env:ControlPlaneSubscriptionID
-$DevSubscriptionID = $Env:DevSubscriptionID
-$ControlPlaneSubscriptionName = $Env:ControlPlaneSubscriptionName
-$DevSubscriptionName = $Env:DevSubscriptionName
-
-if ($ControlPlaneSubscriptionID.Length -eq 0) {
-  Write-Host "ControlPlaneSubscriptionID is not set"
-  exit
+#----------------------
+# Set $ControlPlaneSubscriptionID
+#   Exit     or    $Env:ControlPlaneSubscriptionID
+#----------------------
+if ($Env:ControlPlaneSubscriptionID.Length -ne 0) {
+  $ControlPlaneSubscriptionID = $Env:ControlPlaneSubscriptionID
 }
+else { Write-Host "ControlPlaneSubscriptionID is not set"; exit }
+Write-Host "ControlPlaneSubscriptionID:     $ControlPlaneSubscriptionID"
+#----------------------
 
-if ($ControlPlaneSubscriptionName.Length -eq 0) {
-  Write-Host "ControlPlaneSubscriptionName is not set"
-  exit
+#----------------------
+# Set $ControlPlaneSubscriptionName
+#   Exit     or    $Env:ControlPlaneSubscriptionName
+#----------------------
+if ($Env:ControlPlaneSubscriptionName.Length -ne 0) {
+  $ControlPlaneSubscriptionName = $Env:ControlPlaneSubscriptionName
 }
+else { Write-Host "ControlPlaneSubscriptionName is not set"; exit }
+Write-Host "ControlPlaneSubscriptionName:   $ControlPlaneSubscriptionName"
+#----------------------
 
-if ($DevSubscriptionID.Length -eq 0) {
-  Write-Host "DevSubscriptionID is not set"
-  exit
+#----------------------
+# Set $DevSubscriptionID
+#   Exit     or    $Env:DevSubscriptionID
+#----------------------
+if ($Env:DevSubscriptionID.Length -ne 0) {
+  $DevSubscriptionID = $Env:DevSubscriptionID
 }
+else { Write-Host "DevSubscriptionID is not set"; exit }
+Write-Host "DevSubscriptionID:              $DevSubscriptionID"
+#----------------------
 
-if ($DevSubscriptionName.Length -eq 0) {
-  Write-Host "DevSubscriptionName is not set"
-  exit
+#----------------------
+# Set $DevSubscriptionName
+#   Exit     or    $Env:DevSubscriptionName
+#----------------------
+if ($Env:DevSubscriptionName.Length -ne 0) {
+  $DevSubscriptionName = $Env:DevSubscriptionName
 }
+else { Write-Host "DevSubscriptionName is not set"; exit }
+Write-Host "DevSubscriptionName:            $DevSubscriptionName"
+#----------------------
 
 
+$POOL=$SDAF_CONTROL_PLANE_CODE+"-POOL"
+
+
+Write-Host
+
+Write-Host "Set Defaults..."
 az config set extension.use_dynamic_install=yes_without_prompt --only-show-errors
-
 az devops configure --defaults organization=$Env:ADO_ORGANIZATION project=$Env:ADO_PROJECT
+
+Write-Host "Perform Login..."
 az login --scope https://graph.microsoft.com//.default
 
+Write-Host "Validate Organization URL..."
 $url = ( az devops project list --organization $Organization --query "value | [0].url")
-if ($url.Length -eq 0) {
-  Write-Host "Could not get the DevOps organization URL"
-  exit
-}
+if ($url.Length -eq 0) { Write-Host "Could not get the DevOps organization URL"; exit }
+else { $url_substr = $url.Substring(0, $url.IndexOf("_api")) }
+Write-Host "  url_substr:       $url_substr"
 
-$idx = $url.IndexOf("_api")
-$pat_url = ($url.Substring(0, $idx) + "_usersSettings/tokens").Replace("""", "")
 
+Write-Host "Validate Project ID..."
 $project_id = (az devops project list --organization $Env:ADO_ORGANIZATION --query "[value[]] | [0] | [? name=='$Env:ADO_PROJECT'].id | [0]").Replace("""", "")
-$permissions_url = $url.Substring(0, $idx) + $project_id + "/_settings/repositories?_a=permissions"
+if ($project_id.Length -eq 0) { Write-Host "Could not get the DevOps Project ID"; exit }
+Write-Host "  project_id:       $project_id"
+
+
+$pat_url = ($url_substr + "_usersSettings/tokens").Replace("""", "")
+$permissions_url = ($url_substr + $project_id + "/_settings/repositories?_a=permissions").Replace("""", "")
+$pool_url = ($url_substr + "_settings/agentpools").Replace("""", "")
+Write-Host "Defining URL's..."
+Write-Host "  pat_url:         $pat_url"
+Write-Host "  permissions_url: $permissions_url"
+Write-Host "  pool_url:        $pool_url"
+
+
+Write-Host "Getting Group ID for: $MgmtPrefix"
 $GroupID = (az pipelines variable-group list --project $Project --organization $Organization --query "[?name=='$MgmtPrefix'].id | [0]" --only-show-errors )
-if ($GroupID.Length -eq 0) {
-  Write-Host "Could not find variable group SDAF-MGMT"
-  exit
-}
+if ($GroupID.Length -eq 0) { Write-Host "Could not find variable group $MgmtPrefix"; exit }
+Write-Host " Found group ID $GroupID"
 
-Write-Host "Found group ID $GroupID"
 
+Write-Host "Checking PAT Secret..."
 $AlreadySet = [Boolean](az pipelines variable-group variable list --group-id $GroupID --query PAT.isSecret --only-show-errors)
-
-if ($AlreadySet) {
-  Write-Host "The PAT is already set"
-}
+if ($AlreadySet) { Write-Host " The PAT is already set" }
 else {
   Write-Host "The browser will now open, please create a Personal Access Token. Ensure that Read & manage is selected for Agent Pools, Read & write is selected for Code, Read & execute is selected for Build, and Read, create, & manage is selected for Variable Groups"
   Start-Process $pat_url
 
   $PAT = Read-Host -Prompt "Enter the PAT you just created"
+  Write-Host "  Setting PAT Secret..."
   az pipelines variable-group variable update --group-id $GroupID --project $Project --organization $Organization --name "PAT" --value $PAT --secret true --only-show-errors
-  <# Action when all if and elseif conditions are false #>
 }
 
-$pool_url = $url.Substring(0, $idx) + "_settings/agentpools"
-$POOL=$SDAF_CONTROL_PLANE_CODE+"-POOL"
 
+Write-Host "Checking Agent Pool: $POOL..."
 $POOL_NAME = $(az pipelines pool list  --organization $Organization  --query "[?name=='$POOL'].name | [0]")
-if ($POOL_NAME.Length -gt 0) {
-  Write-Host "Agent pool " $POOL " already exists"
-}
+if ($POOL_NAME.Length -ne 0) { Write-Host " Agent pool " $POOL " already exists" }
 else {
   Write-Host "The browser will now open, please create an Agent Pool with the name '$POOL'. Ensure that the Agent Pool is define using the Self-hosted pool type."
 
-  Start-Process $pool_url.Replace("""", "")
+  Start-Process $pool_url
   Read-Host -Prompt "Once you have created the Agent pool, Press any key to continue"
 }
 
+
+Write-Host "Build Service Permissions..."
 Write-Host "The browser will now open, please ensure that the '" $Env:ADO_PROJECT " Build Service' has 'Allow' in the Contribute section"
 
-Start-Process $permissions_url.Replace("""", "")
+Start-Process $permissions_url
 Read-Host -Prompt "Once you have verified the permission, Press any key to continue"
+
 
 Write-Host "Creating the App registration in Azure Active Directory"
 
