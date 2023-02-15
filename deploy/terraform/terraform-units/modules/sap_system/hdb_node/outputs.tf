f7e657def981243cf5839c92e5e8d09cb3bcaf5e
Kimmo Forss <kimforss@microsoft.com>
2023-02-15 11:42:15 +0200

    Get the last list item

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/outputs.tf
@@ -111,7 +111,7 @@ output "db_admin_ip" {
 }
 
 output "db_ip" {
-  value = local.enable_deployment ? try(azurerm_network_interface.nics_dbnodes_db[*].private_ip_addresses[var.use_secondary_ips ? 1 : 0], []) : []
+  value = local.enable_deployment ? element(azurerm_network_interface.nics_dbnodes_db[*].private_ip_addresses, length(azurerm_network_interface.nics_dbnodes_db[*].private_ip_addresses) - 1) : []
 }
 
 output "hana_data_primary" {
