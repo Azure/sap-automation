f79a50438f285cc2c9062c20f5b50954943048d0
hdamecharla <hdamecharla@microsoft.com>
2023-02-15 13:13:40 +0530

    print the VM Id of scs vm for better troubleshooting.

--- deploy/pipelines/templates/acss-registration.sh
+++ deploy/pipelines/templates/acss-registration.sh
@@ -76,9 +76,7 @@ init -upgrade=true                                                              
 echo -e "$green--- Successfully configured the backend "azurerm"! Terraform will automatically use this backend unless the backend configuration changes. ---$reset"
 
 # Fetch values from Terraform State file
-# have awk only fetch the first line of the output: NR==1
-acss_scs_vm_ids=$(    terraform -chdir="${__moduleDir}" output scs_vm_ids)
-echo -e "$green--- SCS VM IDs: $acss_scs_vm_ids ---$reset"
+# have awk only fetch the first line of the output: NR==2
 acss_scs_vm_id=$(     terraform -chdir="${__moduleDir}" output scs_vm_ids                  | awk -F\" 'NR==2{print $2}' | tr -d '\n\r\t[:space:]')
 echo -e "$green--- SCS VM ID: $acss_scs_vm_id ---$reset"
 acss_sid=$(           terraform -chdir="${__moduleDir}" output sid                         | tr -d '"')
