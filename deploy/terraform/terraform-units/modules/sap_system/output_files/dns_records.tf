e89849d076ec084e9f9a41f98875495689c89d86
Kimmo Forss <kimforss@microsoft.com>
2023-02-15 22:46:53 +0200

    Use the correct zone name DOH :)

--- deploy/terraform/terraform-units/modules/sap_system/output_files/dns_records.tf
+++ deploy/terraform/terraform-units/modules/sap_system/output_files/dns_records.tf
@@ -1,7 +1,7 @@
 resource "azurerm_private_dns_a_record" "app_secondary" {
   count               = !var.use_custom_dns_a_registration && length(var.dns) > 0 ? length(var.naming.virtualmachine_names.APP_SECONDARY_DNSNAME) : 0
   name                = var.naming.virtualmachine_names.APP_SECONDARY_DNSNAME[count.index]
-  zone_name           = length(var.dns)
+  zone_name           = var.dns
   resource_group_name = var.management_dns_resourcegroup_name
   ttl                 = 3600
   records             = [var.application_server_secondary_ips[count.index]]
@@ -16,7 +16,7 @@ resource "azurerm_private_dns_a_record" "app_secondary" {
 resource "azurerm_private_dns_a_record" "scs_secondary" {
   count               = !var.use_custom_dns_a_registration && length(var.dns) > 0 ? length(var.naming.virtualmachine_names.SCS_SECONDARY_DNSNAME) : 0
   name                = var.naming.virtualmachine_names.SCS_SECONDARY_DNSNAME[count.index]
-  zone_name           = length(var.dns)
+  zone_name           = var.dns
   resource_group_name = var.management_dns_resourcegroup_name
   ttl                 = 3600
   records             = [var.scs_server_secondary_ips[count.index]]
@@ -31,7 +31,7 @@ resource "azurerm_private_dns_a_record" "scs_secondary" {
 resource "azurerm_private_dns_a_record" "web_secondary" {
   count               = !var.use_custom_dns_a_registration && length(var.dns) > 0 ? length(var.naming.virtualmachine_names.WEB_SECONDARY_DNSNAME) : 0
   name                = var.naming.virtualmachine_names.WEB_SECONDARY_DNSNAME[count.index]
-  zone_name           = length(var.dns)
+  zone_name           = var.dns
   resource_group_name = var.management_dns_resourcegroup_name
   ttl                 = 3600
   records             = [var.webdispatcher_server_secondary_ips[count.index]]
@@ -46,7 +46,7 @@ resource "azurerm_private_dns_a_record" "web_secondary" {
 resource "azurerm_private_dns_a_record" "db_secondary" {
   count               = !var.use_custom_dns_a_registration && length(var.dns) > 0 ? length(local.db_secondary_dns_names) : 0
   name                = local.db_secondary_dns_names[count.index]
-  zone_name           = length(var.dns)
+  zone_name           = var.dns
   resource_group_name = var.management_dns_resourcegroup_name
   ttl                 = 3600
   records             = [var.db_server_secondary_ips[count.index]]
