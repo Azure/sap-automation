6ecb61589b1fba6bf38a13a918f373865ca3d2e3
Kimmo Forss <kimforss@microsoft.com>
2023-02-27 12:27:17 +0200

    missing &&

--- deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
@@ -355,8 +355,12 @@ resource "azurerm_storage_share" "transport" {
 
   name = format("%s", local.resource_suffixes.transport_volume)
 
-  storage_account_name = length(var.transport_storage_account_id) > 0 split("/",var.transport_storage_account_id)[8] : azurerm_storage_account.transport[0].name
-  enabled_protocol     = "NFS"
+  storage_account_name = length(var.transport_storage_account_id) > 0 ? (
+    split("/", var.transport_storage_account_id)[8]
+    ) : (
+    azurerm_storage_account.transport[0].name
+  )
+  enabled_protocol = "NFS"
 
   quota = var.transport_volume_size
 }
