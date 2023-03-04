38d2db61d1ff3e337d058ff9dad11b5e2d60a6da
Kimmo Forss <kimforss@microsoft.com>
2023-03-04 10:32:03 +0200

    use var.environment

--- deploy/terraform/bootstrap/sap_deployer/imports.tf
+++ deploy/terraform/bootstrap/sap_deployer/imports.tf
@@ -1,20 +1,20 @@
 data "azurerm_key_vault_secret" "subscription_id" {
-  name         = format("%s-subscription-id", upper(local.infrastructure.environment))
+  name         = format("%s-subscription-id", upper(var.environment))
   key_vault_id = var.key_vault.kv_exists ? var.key_vault.kv_user_id : azurerm_key_vault.kv_user[0].id
 }
 
 data "azurerm_key_vault_secret" "client_id" {
-  name         = format("%s-client-id", upper(local.infrastructure.environment))
+  name         = format("%s-client-id", upper(var.environment))
   key_vault_id = var.key_vault.kv_exists ? var.key_vault.kv_user_id : azurerm_key_vault.kv_user[0].id
 }
 
 data "azurerm_key_vault_secret" "client_secret" {
-  name         = format("%s-client-secret", upper(local.infrastructure.environment))
+  name         = format("%s-client-secret", upper(var.environment))
   key_vault_id = var.key_vault.kv_exists ? var.key_vault.kv_user_id : azurerm_key_vault.kv_user[0].id
 }
 
 data "azurerm_key_vault_secret" "tenant_id" {
-  name         = format("%s-tenant-id", upper(local.infrastructure.environment))
+  name         = format("%s-tenant-id", upper(var.environment))
   key_vault_id = var.key_vault.kv_exists ? var.key_vault.kv_user_id : azurerm_key_vault.kv_user[0].id
 }
 
