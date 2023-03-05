e36885713d597a1181e44a3d3cba3afb94e99dc3
Kimmo Forss <kimforss@microsoft.com>
2023-03-05 04:19:11 +0200

    cleanup

--- deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
@@ -415,11 +415,3 @@ data "azurerm_network_interface" "storage_sapbits" {
   name                = azurerm_private_endpoint.storage_sapbits[count.index].network_interface[0].name
   resource_group_name = split("/", azurerm_private_endpoint.storage_sapbits[count.index].network_interface[0].id)[4]
 }
-
-
-resource "azurerm_role_assignment" "storage_tfstate" {
-  name               = "00000000-0000-0000-0000-000000000000"
-  scope              = data.azurerm_subscription.primary.id
-  role_definition_id = azurerm_role_definition.example.role_definition_resource_id
-  principal_id       = data.azurerm_client_config.example.object_id
-}
