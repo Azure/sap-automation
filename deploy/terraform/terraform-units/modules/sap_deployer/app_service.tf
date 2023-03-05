6f737a9a67b6ba57c1dd106739e27b2ac0f703df
Kimmo Forss <kimforss@microsoft.com>
2023-03-05 18:22:28 +0200

    authentication changes

--- deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
@@ -68,25 +68,26 @@ resource "azurerm_windows_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.appserviceplan[0].id
 
   auth_settings_v2 {
-    auth_enabled = true
+    auth_enabled           = true
     unauthenticated_action = "RedirectToLoginPage"
-    default_provider = "AzureActiveDirectory"
-    active_directory_v2{
-      client_id = var.app_registration_app_id
-      login_parameters = {}
-      tenant_auth_endpoint = format("https://login.microsoftonline.com/v2.0/%s/", data.azurerm_client_config.deployer.tenant_id)
-      www_authentication_disabled  = false
-      client_secret_setting_name = format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/WEB-PWD/)", local.keyvault_names.user_access)
+    default_provider       = "AzureActiveDirectory"
+    active_directory_v2 {
+      client_id                   = var.app_registration_app_id
+      login_parameters            = {}
+      tenant_auth_endpoint        = format("https://login.microsoftonline.com/v2.0/%s/", data.azurerm_client_config.deployer.tenant_id)
+      www_authentication_disabled = false
+      client_secret_setting_name  = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
     }
-    login{
+    login {
       token_store_enabled = true
     }
   }
 
   app_settings = {
-    "PAT"                    = format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/PAT/)", local.keyvault_names.user_access)
-    "CollectionUri"          = var.agent_ado_url
-    "IS_PIPELINE_DEPLOYMENT" = false
+    "PAT"                                      = format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/PAT/)", local.keyvault_names.user_access)
+    "CollectionUri"                            = var.agent_ado_url
+    "IS_PIPELINE_DEPLOYMENT"                   = false
+    "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET" = format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/WEB-PWD/)", local.keyvault_names.user_access)
   }
 
   virtual_network_subnet_id = local.webapp_subnet_exists ? data.azurerm_subnet.webapp[0].id : azurerm_subnet.webapp[0].id
