9d48a45989b5fcf6ded4ce00e22e466945d5e7cf
Kimmo Forss <kimforss@microsoft.com>
2023-03-07 22:52:30 +0200

    remove the data source

--- deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
@@ -706,19 +706,6 @@ resource "time_sleep" "wait_for_private_endpoints" {
   ]
 }
 
-data "azurerm_private_dns_zone" "storage" {
-  provider            = azurerm.dnsmanagement
-  count               = var.use_private_endpoint && !var.use_custom_dns_a_registration ? 1 : 0
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = var.management_dns_resourcegroup_name
-}
-
-data "azurerm_private_dns_zone" "file" {
-  provider            = azurerm.dnsmanagement
-  count               = var.use_private_endpoint && !var.use_custom_dns_a_registration ? 1 : 0
-  name                = "privatelink.file.core.windows.net"
-  resource_group_name = var.management_dns_resourcegroup_name
-}
 
 data "azurerm_network_interface" "storage_bootdiag" {
   provider            = azurerm.main
