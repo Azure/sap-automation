e40165d625284a3a0b1b16da45a6eabfa75f7526
Kimmo Forss <kimforss@microsoft.com>
2023-03-07 23:03:31 +0200

    DNS updates

--- deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
@@ -378,7 +378,7 @@ data "azurerm_storage_account" "transport" {
 }
 
 resource "azurerm_private_endpoint" "transport" {
-  provider = azurerm.main
+  provider = azurerm.dnsmanagement
   depends_on = [
     azurerm_subnet.app
   ]
@@ -588,7 +588,7 @@ data "azurerm_private_endpoint_connection" "install" {
 }
 
 resource "azurerm_private_endpoint" "install" {
-  provider = azurerm.main
+  provider = azurerm.dnsmanagement
 
   depends_on = [
     azurerm_subnet.app
