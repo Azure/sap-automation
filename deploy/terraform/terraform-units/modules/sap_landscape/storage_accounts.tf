f5ef6456906d39d55749f32b996fdd32e3897fff
Kimmo Forss <kimforss@microsoft.com>
2023-03-10 00:09:52 +0200

    fix the if statement

--- deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
@@ -685,14 +685,14 @@ resource "time_sleep" "wait_for_private_endpoints" {
 
 data "azurerm_network_interface" "storage_bootdiag" {
   provider            = azurerm.main
-  count               = var.use_private_endpoint && length(var.diagnostics_storage_account.arm_id) == 0  && length(try(azurerm_private_endpoint.storage_bootdiag[0].network_interface[0].id, "")) ? 1 : 0
+  count               = var.use_private_endpoint && length(var.diagnostics_storage_account.arm_id) == 0 && length(try(azurerm_private_endpoint.storage_bootdiag[0].network_interface[0].id, "")) > 0 ? 1 : 0
   name                = azurerm_private_endpoint.storage_bootdiag[count.index].network_interface[0].name
   resource_group_name = split("/", azurerm_private_endpoint.storage_bootdiag[count.index].network_interface[0].id)[4]
 }
 
 data "azurerm_network_interface" "witness_storage" {
   provider            = azurerm.main
-  count               = var.use_private_endpoint && length(var.witness_storage_account.arm_id) == 0 && length(try(azurerm_private_endpoint.witness_storage[0].network_interface[0].id, "")) ? 1 : 0
+  count               = var.use_private_endpoint && length(var.witness_storage_account.arm_id) == 0 && length(try(azurerm_private_endpoint.witness_storage[0].network_interface[0].id, "")) > 0 ? 1 : 0
   name                = azurerm_private_endpoint.witness_storage[count.index].network_interface[0].name
   resource_group_name = split("/", azurerm_private_endpoint.witness_storage[count.index].network_interface[0].id)[4]
 }
