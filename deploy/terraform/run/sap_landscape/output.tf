4f7bf66764b60d1b6a1b4e7fc2520ba9b08e7054
Kimmo Forss <kimforss@microsoft.com>
2023-03-09 18:11:10 +0200

    fix outputs

--- deploy/terraform/run/sap_landscape/output.tf
+++ deploy/terraform/run/sap_landscape/output.tf
@@ -176,8 +176,12 @@ output "dns_label" {
 
 output "use_custom_dns_a_registration" {
   description = "Defines if custom DNS is used"
-  value       = var.use_custom_dns_a_registration || !(
-    (var.management_dns_subscription_id != data.azurerm_key_vault_secret.subscription_id.value) || (var.management_dns_resourcegroup_name != (local.sa_tfstate_name))
+  value = var.use_custom_dns_a_registration || !(
+    (
+      var.management_dns_subscription_id != data.azurerm_key_vault_secret.subscription_id.value
+      ) || (
+      var.management_dns_resourcegroup_name != (local.saplib_resource_group_name)
+    )
   )
 }
 
