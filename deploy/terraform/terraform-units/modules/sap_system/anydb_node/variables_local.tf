cf03cb058e6b787d1c99e8d27b0e1a565d0b8348
Kimmo Forss <kimforss@microsoft.com>
2023-03-13 00:58:55 +0200

    HS IP for Windows load balancer

--- deploy/terraform/terraform-units/modules/sap_system/anydb_node/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/anydb_node/variables_local.tf
@@ -476,10 +476,10 @@ locals {
     }
   ]
 
-  fpips = (var.database.high_availability && upper(local.anydb_ostype) == "WINDOWS") ? (
-    compact(concat(local.std_ips, local.winha_ips))) : (
-    local.std_ips
-  )
+  fpips = compact((var.database.high_availability && upper(local.anydb_ostype) == "WINDOWS") ? (
+    concat(local.std_ips, local.winha_ips)) : (
+    concat(local.std_ips,[])
+  ))
 
 
 }
