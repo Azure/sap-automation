141e743bad2ad7bffc18c6048973847261d6b822
Kimmo Forss <kimforss@microsoft.com>
2023-03-13 04:55:54 +0200

    use the second IP

--- deploy/terraform/terraform-units/modules/sap_system/anydb_node/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/anydb_node/outputs.tf
@@ -41,8 +41,8 @@ output "db_lb_ip" {
 }
 
 output "db_clst_lb_ip" {
-  value = local.enable_db_lb_deployment && (var.use_loadbalancers_for_standalone_deployments && local.winHA || local.anydb_ha && local.winHA) ? (
-    try(azurerm_lb.anydb[0].frontend_ip_configuration[1].private_ip_address, "")) : (
+  value = (local.enable_db_lb_deployment && (var.use_loadbalancers_for_standalone_deployments && local.winHA || local.anydb_ha) && local.winHA) ? (
+    try(azurerm_lb.anydb[0].frontend_ip_configuration.private_ip_addresses[1], "")) : (
     ""
   )
 
