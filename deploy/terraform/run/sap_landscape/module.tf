133c775bc824ed04c4a955cfed731d56604580c2
Kimmo Forss <kimforss@microsoft.com>
2023-03-30 06:19:43 +0300

    lowercase DNS

--- deploy/terraform/run/sap_landscape/module.tf
+++ deploy/terraform/run/sap_landscape/module.tf
@@ -36,10 +36,10 @@ module "sap_landscape" {
   create_vaults_and_storage_dns_a_records = var.create_vaults_and_storage_dns_a_records
   use_custom_dns_a_registration           = var.use_custom_dns_a_registration
   management_dns_subscription_id          = coalesce(var.management_dns_subscription_id, local.saplib_subscription_id)
-  management_dns_resourcegroup_name = length(var.management_dns_resourcegroup_name) > 0 ? (
+  management_dns_resourcegroup_name = lower(length(var.management_dns_resourcegroup_name) > 0 ? (
     var.management_dns_resourcegroup_name) : (
     local.saplib_resource_group_name
-  )
+  ))
 
   Agent_IP = var.Agent_IP
 
