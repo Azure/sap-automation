17ec660185b830063b8e252a30d83bfdd8e04e3c
Kimmo Forss <kimforss@microsoft.com>
2023-04-21 19:44:42 +0300

    Update terraform version

--- deploy/terraform/bootstrap/sap_deployer/tfvar_variables.tf
+++ deploy/terraform/bootstrap/sap_deployer/tfvar_variables.tf
@@ -336,7 +336,7 @@ variable "deployer_diagnostics_account_arm_id" {
 
 variable "tf_version" {
   description = "Terraform version to install on deployer"
-  default     = "1.4.1"
+  default     = "1.4.5"
 }
 
 variable "name_override_file" {
