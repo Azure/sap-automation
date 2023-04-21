a59a1eaaf2390c97cb70b8edceda6a19eaad0a60
Kimmo Forss <kimforss@microsoft.com>
2023-04-21 11:37:04 +0300

    update the Windows VMs as well

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-app.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-app.tf
@@ -122,7 +122,6 @@ resource "azurerm_linux_virtual_machine" "app" {
   location            = var.resource_group[0].location
   resource_group_name = var.resource_group[0].name
 
-  //If no ppg defined do not put the application servers in a proximity placement group
   proximity_placement_group_id = var.application_tier.scs_use_ppg ? (
     local.app_zonal_deployment ? var.ppg[count.index % max(local.app_zone_count, 1)].id : var.ppg[0].id) : (
     null
@@ -255,20 +254,20 @@ resource "azurerm_windows_virtual_machine" "app" {
   location            = var.resource_group[0].location
   resource_group_name = var.resource_group[0].name
 
-  //If no ppg defined do not put the application servers in a proximity placement group
-  proximity_placement_group_id = local.app_no_ppg ? (
-    null) : (
-    local.app_zonal_deployment ? var.ppg[count.index % max(local.app_zone_count, 1)].id : var.ppg[0].id
+  proximity_placement_group_id = var.application_tier.scs_use_ppg ? (
+    local.app_zonal_deployment ? var.ppg[count.index % max(local.app_zone_count, 1)].id : var.ppg[0].id) : (
+    null
   )
 
   //If more than one servers are deployed into a single zone put them in an availability set and not a zone
-  availability_set_id = local.use_app_avset ? (
+  availability_set_id = var.application_tier.app_use_avset ? (
     length(var.application_tier.avset_arm_ids) > 0 ? (
       var.application_tier.avset_arm_ids[count.index % max(local.app_zone_count, 1)]) : (
       azurerm_availability_set.app[count.index % max(local.app_zone_count, 1)].id
     )) : (
     null
   )
+
   //If length of zones > 1 distribute servers evenly across zones
   zone = local.use_app_avset ? null : try(local.app_zones[count.index % max(local.app_zone_count, 1)], null)
 
