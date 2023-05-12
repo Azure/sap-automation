7208009f74ac790540be3f2fee9501d38bf567f8
Kimmo Forss <kimforss@microsoft.com>
2023-05-12 20:33:12 +0300

    simplify the data source

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/avg.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/avg.tf
@@ -30,7 +30,7 @@ resource "azapi_resource" "avg_HANA" {
 data "azurerm_netapp_pool" "workload_netapp_pool" {
   provider            = azurerm.main
   count               = var.NFS_provider == "ANF" ? 1 : 1
-  resource_group_name = split("/", local.ANF_pool_settings.account_id)[4]
+  resource_group_name = data.azurerm_netapp_account.workload_netapp_account[0].resource_group_name
   name                = local.ANF_pool_settings.pool_name
   account_name        = data.azurerm_netapp_account.workload_netapp_account[0].name
 
@@ -39,8 +39,8 @@ data "azurerm_netapp_pool" "workload_netapp_pool" {
 data "azurerm_netapp_account" "workload_netapp_account" {
   provider            = azurerm.main
   count               = var.NFS_provider == "ANF" ? 1 : 1
-  name                = split("/", local.ANF_pool_settings.account_id)[8]
-  resource_group_name = split("/", local.ANF_pool_settings.account_id)[4]
+  name                = try(split("/", local.ANF_pool_settings.account_id)[8], "")
+  resource_group_name = try(split("/", local.ANF_pool_settings.account_id)[4], "")
 }
 
 
