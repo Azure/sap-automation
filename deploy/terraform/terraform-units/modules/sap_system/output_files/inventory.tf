1181cc823d4929e4e3aba110c48aa70df2b54a4a
Kimmo Forss <kimforss@microsoft.com>
2023-05-13 22:05:06 +0300

    make sapmnt multi value

--- deploy/terraform/terraform-units/modules/sap_system/output_files/inventory.tf
+++ deploy/terraform/terraform-units/modules/sap_system/output_files/inventory.tf
@@ -142,9 +142,10 @@ resource "local_file" "sap-parameters_yml" {
     dns = local.dns_label
     bom = local.bom
 
-    sap_mnt = length(var.sap_mnt) == 1 ? (
-      format("sap_mnt:                       %s", var.sap_mnt[0])) : (
+    sap_mnt = length(trimspace(var.sap_mnt[0])) > 0 ? (
       format("sap_mnt:                       %s", jsonencode(var.sap_mnt))
+      ) : (
+      ""
     )
 
     sap_transport = length(trimspace(var.sap_transport)) > 0 ? (
