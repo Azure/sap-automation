c19ec30a83183da8025358ce09749d0d51623cda
Kimmo Forss <kimforss@microsoft.com>
2023-10-30 22:03:57 +0200

    Library code formatting

--- deploy/terraform/bootstrap/sap_library/output.tf
+++ deploy/terraform/bootstrap/sap_library/output.tf
@@ -1,24 +1,23 @@
+#######################################4#######################################8
+#                                                                              #
+#                          Resource group definitioms                          #
+#                                                                              #
+#######################################4#######################################8
 
-###############################################################################
-#                                                                             #
-#                             Resource Group                                  #
-#                                                                             #
-###############################################################################
+output "created_resource_group_id"               {
+                                                   description = "Created resource group ID"
+                                                   value       = module.sap_library.created_resource_group_id
+                                                 }
 
-output "created_resource_group_id" {
-  description = "Created resource group ID"
-  value       = module.sap_library.created_resource_group_id
-}
+output "created_resource_group_name"             {
+                                                   description = "Created resource group name"
+                                                   value       = module.sap_library.created_resource_group_name
+                                                 }
 
-output "created_resource_group_subscription_id" {
-  description = "Created resource group' subscription ID"
-  value       = module.sap_library.created_resource_group_subscription_id
-}
-
-output "created_resource_group_name" {
-  description = "Created resource group name"
-  value       = module.sap_library.created_resource_group_name
-}
+output "created_resource_group_subscription_id"  {
+                                                   description = "Created resource group' subscription ID"
+                                                   value       = module.sap_library.created_resource_group_subscription_id
+                                                 }
 
 
 ###############################################################################
@@ -27,46 +26,47 @@ output "created_resource_group_name" {
 #                                                                             #
 ###############################################################################
 
-output "sapbits_storage_account_name" {
-  description = "Storage account name for SAP Binaries"
-  value       = module.sap_library.sapbits_storage_account_name
-}
+output "sapbits_storage_account_name"           {
+                                                   description = "Storage account name for SAP Binaries"
+                                                   value       = module.sap_library.sapbits_storage_account_name
+                                                 }
 
-output "sapbits_sa_resource_group_name" {
-  description = "Resource group name for SAP Binaries"
-  value       = module.sap_library.sapbits_sa_resource_group_name
-}
+output "sapbits_sa_resource_group_name"          {
+                                                   description = "Resource group name for SAP Binaries"
+                                                   value       = module.sap_library.sapbits_sa_resource_group_name
+                                                 }
 
-output "saplibrary_subscription_id" {
-  description = "Subscription Id for SAP Binaries"
-  sensitive   = true
-  value       = local.spn.subscription_id
-}
+output "saplibrary_subscription_id"              {
+                                                   description = "Subscription Id for SAP Binaries"
+                                                   sensitive   = true
+                                                   value       = local.spn.subscription_id
+                                                 }
 
-output "tfstate_resource_id" {
-  description = "Azure Resource identifier for Terraform remote state"
-  value       = module.sap_library.tfstate_resource_id
-}
+output "tfstate_resource_id"                     {
+                                                   description = "Azure Resource identifier for Terraform remote state"
+                                                   value       = module.sap_library.tfstate_resource_id
+                                                 }
 
-output "remote_state_storage_account_name" {
-  description = "Storage account name for Terraform remote state"
-  value       = module.sap_library.remote_state_storage_account_name
-}
+output "remote_state_storage_account_name"       {
+                                                   description = "Storage account name for Terraform remote state"
+                                                   value       = module.sap_library.remote_state_storage_account_name
+                                                 }
 
-output "saplibrary_environment" {
-  description = "Name of enfironment"
-  value       = local.infrastructure.environment
-}
+output "saplibrary_environment"                  {
+                                                   description = "Name of enfironment"
+                                                   value       = local.infrastructure.environment
+                                                 }
 
-output "automation_version" {
-  value = local.version_label
-}
+output "automation_version"                      {
+                                                   description = "Version of automation"
+                                                   value = local.version_label
+                                                 }
 
-output "sa_connection_string" {
-  description = "Connection string to storage account"
-  sensitive   = true
-  value       = module.sap_library.sa_connection_string
-}
+output "sa_connection_string"                    {
+                                                   description = "Connection string to storage account"
+                                                   sensitive   = true
+                                                   value       = module.sap_library.sa_connection_string
+                                                 }
 
 
 ###############################################################################
@@ -75,7 +75,8 @@ output "sa_connection_string" {
 #                                                                             #
 ###############################################################################
 
-output "random_id_b64" {
-  value = module.sap_library.random_id_b64
-}
+output "random_id_b64"                          {
+                                                   description = "Random ID in base64"
+                                                   value      = module.sap_library.random_id_b64
+                                                 }
 
