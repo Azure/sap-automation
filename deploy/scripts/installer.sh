33509698a3ee15fd8b84450f5a8e7c6b8218ff77
Kimmo Forss <kimforss@microsoft.com>
2024-01-24 20:38:20 +0200

    Fix ARM_CLIENT_SECRET variable in installer.sh

--- deploy/scripts/installer.sh
+++ deploy/scripts/installer.sh
@@ -1133,7 +1133,7 @@ then
     echo ""
     echo ""
 
-    if [ -n ${ARM_CLIENT_SECRET} ] ; then
+    if [ -n "${ARM_CLIENT_SECRET}" ] ; then
       az login --service-principal --username "${ARM_CLIENT_ID}" --password=$ARM_CLIENT_SECRET --tenant "${ARM_TENANT_ID}"  --output none
     else
       az login --identity --output none
