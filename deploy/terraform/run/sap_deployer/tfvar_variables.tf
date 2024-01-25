3fe7ab6dca82ba1fe16c67366326112b067165c9
Kimmo Forss <kimforss@microsoft.com>
2024-01-26 00:28:18 +0200

    Add deployment variable to tfvar_variables.tf

--- deploy/terraform/run/sap_deployer/tfvar_variables.tf
+++ deploy/terraform/run/sap_deployer/tfvar_variables.tf
@@ -403,6 +403,11 @@ variable "subnets_to_add_to_firewall_for_keyvaults_and_storage" {
                                                                   default     = []
                                                                 }
 
+variable "deployment"                           {
+                                                  description = "The type of deployment"
+                                                  default     = "update"
+                                                }
+
 #########################################################################################
 #                                                                                       #
 #  DNS settings                                                                         #
