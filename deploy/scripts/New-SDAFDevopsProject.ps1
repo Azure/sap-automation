293b87cc4b4ff00fd8ecaff4e081a76257f65725
Kimmo Forss <kimforss@microsoft.com>
2024-02-07 18:13:16 +0200

    Fix repository creation and import bug

--- deploy/scripts/New-SDAFDevopsProject.ps1
+++ deploy/scripts/New-SDAFDevopsProject.ps1
@@ -232,12 +232,11 @@ else {
   $repo_id = (az repos list --query "[?name=='$ADO_Project'].id | [0]" --out tsv)
   if ($repo_id.Length -eq 0) {
     Write-Host "Creating repository '$ADO_Project'" -ForegroundColor Green
-    $repo_id = (az repos create --name $ADO_Project --query id --output tsv)
   }
 
   az devops configure --defaults organization=$ADO_ORGANIZATION project=$ADO_PROJECT
 
-  $repo_size = (az repos list --query "[?id=='$repo_id'].size | [0]")
+  $repo_size = (az repos list --query "[?name=='$ADO_Project'].size | [0]")
 
   if ($repo_size -eq 0) {
     Write-Host "Importing the repository from GitHub" -ForegroundColor Green
