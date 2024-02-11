e27b979ea3b4218dc56dc693bba2cbfbcd1d5754
Kimmo Forss <kimforss@microsoft.com>
2024-02-11 18:12:39 +0200

    Refactor storage account tags

--- deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
@@ -62,11 +62,10 @@ resource "azurerm_storage_account" "storage_tfstate" {
               ignore_changes = [tags]
             }
 
-            tags = {
-                "enable_firewall_for_keyvaults_and_storage" = local.enable_firewall_for_keyvaults_and_storage
-                "public_network_access_enabled" = var.storage_account_sapbits.public_network_access_enabled
-              }
-            )
+  tags = {
+      "enable_firewall_for_keyvaults_and_storage" = local.enable_firewall_for_keyvaults_and_storage
+      "public_network_access_enabled" = var.storage_account_sapbits.public_network_access_enabled
+    }
 
 }
 
