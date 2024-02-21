32e2f1d5354806e8ef529933ac5e4168b3ea7aa5
Kimmo Forss <kimforss@microsoft.com>
2024-02-21 19:58:09 +0200

    Update availability set IDs in VM resource configuration

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-webdisp.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-webdisp.tf
@@ -129,7 +129,7 @@ resource "azurerm_linux_virtual_machine" "web" {
 
   //If more than one servers are deployed into a single zone put them in an availability set and not a zone
   availability_set_id                  = local.use_web_avset ? (
-                                           azurerm_availability_set.web[count.index % max(local.web_zone_count, 1)].id
+                                           azurerm_availability_set.web[count.index % max(length(azurerm_availability_set.web), 1)].id
                                            ) : (
                                            null
                                          )
@@ -269,7 +269,7 @@ resource "azurerm_windows_virtual_machine" "web" {
 
   //If more than one servers are deployed into a single zone put them in an availability set and not a zone
   availability_set_id                  = local.use_web_avset ? (
-                                           azurerm_availability_set.web[count.index % max(local.web_zone_count, 1)].id
+                                           azurerm_availability_set.web[count.index % max(length(azurerm_availability_set.web), 1)].id
                                            ) : (
                                            null
                                          )
