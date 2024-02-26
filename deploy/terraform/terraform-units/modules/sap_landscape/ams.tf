974c4fc136b265d5d4e248dc43719cee3e89faf5
Kimmo Forss <kimforss@microsoft.com>
2024-02-26 22:55:20 +0200

    Update ams_instance count and monitorSubnet in ams.tf

--- deploy/terraform/terraform-units/modules/sap_landscape/ams.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/ams.tf
@@ -9,23 +9,23 @@ data "azurerm_subnet" "ams" {
 
 resource "azapi_resource" "ams_instance" {
   type                                  = "Microsoft.Workloads/monitors@2023-04-01"
-  count                                 = local.create_ams_instance ? 1 : 0
+  count                                 = local.create_ams_instance && local.ams_subnet_defined ? 1 : 0
   name                                  = local.ams_instance_name
   location                              = local.region
   parent_id                             = azurerm_resource_group.resource_group[0].id
-  depends_on                            = [ 
+  depends_on                            = [
                                             azurerm_virtual_network.vnet_sap,
                                             azurerm_subnet.ams
                                           ]
   body                                  = jsonencode({
                                             properties = {
-                                              appLocation: local.region,
-                                              routingPreference: "RouteAll",
-                                              logAnalyticsWorkspaceArmId: local.ams_laws_arm_id,
-                                              managedResourceGroupConfiguration: {
-                                                name: "managedrg-ams"
-                                              },
-                                              monitorSubnet: azurerm_subnet.ams[0].id,
-                                            }
+                                                            appLocation: local.region,
+                                                            routingPreference: "RouteAll",
+                                                            logAnalyticsWorkspaceArmId: local.ams_laws_arm_id,
+                                                            managedResourceGroupConfiguration: {
+                                                              name: "managedrg-ams"
+                                                            },
+                                                           monitorSubnet: length(local.ams_subnet_arm_id) > 0 ? local.ams_subnet_arm_id : azurerm_subnet.ams[0].id,
+                                                          }
                                           })
 }
