90ed5f47c4174c4a69b3896200c34eb4ed54cfe8
Kimmo Forss <kimforss@microsoft.com>
2024-03-08 14:23:40 +0200

    Add random password generation for deployer and update dns_label variable description

--- deploy/terraform/terraform-units/modules/sap_deployer/randomid.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/randomid.tf
@@ -2,3 +2,20 @@
 resource "random_id" "deployer" {
   byte_length = 4
 }
+
+
+// Generate random password if password is set as authentication type, and save in KV
+resource "random_password" "deployer" {
+  count                                = (
+                                           local.enable_password
+                                           && !local.pwd_exist
+                                           && try(var.authentication.password, "") == ""
+                                         ) ? 1 : 0
+
+  length                               = 32
+  min_upper                            = 2
+  min_lower                            = 2
+  min_numeric                          = 2
+  special                              = true
+  override_special                     = "_%@"
+}
