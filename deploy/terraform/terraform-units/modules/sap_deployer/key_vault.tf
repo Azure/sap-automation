d7b5738771195ef8a80ec9b514621c61d19235b1
Kimmo Forss <kimforss@microsoft.com>
2024-03-08 12:24:26 +0200

    Update default_action in key_vault.tf

--- deploy/terraform/terraform-units/modules/sap_deployer/key_vault.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/key_vault.tf
@@ -34,7 +34,7 @@ resource "azurerm_key_vault" "kv_user" {
                            content {
 
                               bypass                     = "AzureServices"
-                             default_action             = local.management_subnet_exists ? "Allow" : "Deny"
+                              default_action             = var.enable_firewall_for_keyvaults_and_storage ? "Deny" : "Allow"
 
                               ip_rules                   = compact(
                                                             [
