9a400b37c2ce550688dbbc64d0cae32fc2212495
hdamecharla <hdamecharla@microsoft.com>
2024-03-19 17:11:22 +0530

    Refactor subnet_storage configuration in transform.tf

--- deploy/terraform/run/sap_system/transform.tf
+++ deploy/terraform/run/sap_system/transform.tf
@@ -531,6 +531,34 @@ locals {
                                            )
                                          )
 
+  subnet_storage                       = merge(
+                                           (
+                                             {
+                                               "name" = try(var.infrastructure.vnets.sap.subnet_storage.name, var.storage_subnet_name)
+                                             }
+                                             ), (
+                                             local.subnet_storage_arm_id_defined ? (
+                                               {
+                                                 "arm_id" = try(var.infrastructure.vnets.sap.subnet_storage.arm_id, var.storage_subnet_arm_id)
+                                               }
+                                               ) : (
+                                               null
+                                             )), (
+                                             {
+                                               "prefix" = try(var.infrastructure.vnets.sap.subnet_storage.prefix, var.storage_subnet_address_prefix)
+                                             }
+                                             ), (
+                                             local.subnet_storage_nsg_defined ? (
+                                               {
+                                                 "nsg" = {
+                                                           "name"   = try(var.infrastructure.vnets.sap.subnet_storage.nsg.name, var.storage_subnet_nsg_name)
+                                                           "arm_id" = try(var.infrastructure.vnets.sap.subnet_storage.nsg.arm_id, var.storage_subnet_nsg_arm_id)
+                                                         }
+                                               }
+                                             ) : null
+                                           )
+                                         )
+
   all_subnets                          = merge(local.sap, (
                                           local.subnet_admin_defined ? (
                                             {
@@ -556,6 +584,14 @@ locals {
                                           local.subnet_web_defined ? (
                                             {
                                               "subnet_web" = local.subnet_web
+                                            }
+                                            ) : (
+                                            null
+                                          )
+                                          ), (
+                                          local.subnet_storage_defined ? (
+                                            {
+                                              "subnet_storage" = local.subnet_storage
                                             }
                                             ) : (
                                             null
