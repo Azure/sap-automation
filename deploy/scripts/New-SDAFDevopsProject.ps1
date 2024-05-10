27dfee3dbd06027989f4e257c1b157a371d81559
Kimmo Forss <kimforss@microsoft.com>
2024-05-10 13:46:51 +0300

    Update on devops login

--- deploy/scripts/New-SDAFDevopsProject.ps1
+++ deploy/scripts/New-SDAFDevopsProject.ps1
@@ -44,24 +44,11 @@ $versionLabel = "v3.11.0.2"
 
 if ($Env:AZURE_DEVOPS_EXT_PAT.Length -gt 0) {
   Write-Host "Using the provided Personal Access Token (PAT) to authenticate to the Azure DevOps organization $ADO_Organization" -ForegroundColor Yellow
-  try {
-    az devops login --organization $ADO_Organization
-  }
-  catch {
-    $_
-  }
-
 }
 
 $checkPAT = (az devops user list --organization $ADO_Organization --only-show-errors --top 1)
 if ($checkPAT.Length -eq 0) {
   $env:AZURE_DEVOPS_EXT_PAT = Read-Host "Please enter your Personal Access Token (PAT) with full access to the Azure DevOps organization $ADO_Organization"
-  try {
-    az devops login --organization $ADO_Organization
-  }
-  catch {
-    $_
-  }
   $verifyPAT = (az devops user list --organization $ADO_Organization --only-show-errors --top 1)
   if ($verifyPAT.Length -eq 0) {
     Read-Host -Prompt "Failed to authenticate to the Azure DevOps organization, press <any key> to exit"
