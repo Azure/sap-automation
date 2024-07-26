3b0ef6530d51c69996b429811d86398c40f83792
devanshjain <devanshjain@microsoft.com>
2024-07-26 05:12:28 +0000

    update: add network rules to deployer diagnostic storage account

--- deploy/terraform/terraform-units/modules/sap_deployer/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/infrastructure.tf
@@ -88,6 +88,10 @@ resource "azurerm_storage_account" "deployer" {
   min_tls_version                      = "TLS1_2"
   allow_nested_items_to_be_public      = false
   shared_access_key_enabled            = var.deployer.shared_access_key_enabled
+  network_rules {
+    default_action                     = "Deny"
+    virtual_network_subnet_ids         = [azurerm_subnet.subnet_mgmt[0].id]
+  }
 }
 
 data "azurerm_storage_account" "deployer" {
