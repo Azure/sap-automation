967f50b52f639011b0fbec72f2e74118d742ee74
devanshjain <devanshjain@microsoft.com>
2024-08-07 00:06:57 +0000

    Refactor use_spn variable to default to false for all SAP deployment modules

--- deploy/terraform/run/sap_system/tfvar_variables.tf
+++ deploy/terraform/run/sap_system/tfvar_variables.tf
@@ -351,7 +351,7 @@ variable "automation_path_to_private_key"       {
 
 variable "use_spn"                              {
                                                   description = "Log in using a service principal when performing the deployment"
-                                                  default     = true
+                                                  default     = false
                                                 }
 
 
