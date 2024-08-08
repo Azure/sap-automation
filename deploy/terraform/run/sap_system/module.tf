e7f0eb23b6af5575fba5f70b9f4e76733a8e3506
Devansh Jain <86314060+devanshjainms@users.noreply.github.com>
2024-08-08 10:32:29 -0700

    Hotfix release after testing with new RHEL image (#611)
    * update: added fixed encountered during RHEL94 testing
    
    * chore: Update Red Hat and SLES package versions for Red Hat 9.4
    
    * update: add network rules to deployer diagnostic storage account
    
    * chore: Update sap-vm-resources.tmpl to include additional server information
    
    * chore: Add ${SID}_virtual_machines.json to git if it exists
    
    * chore: Update sap_system module to use database_server_vm_resource_ids for database server VMs
    
    * chore: Update sap_system module to include empty lists for SCS, application, and webdisp server VMs
    
    * chore: Update sap-vm-resources.tmpl to include additional server information
    
    * chore: Update sap-vm-resources.tmpl to include additional server information
    
    * chore: Update sap-vm-resources.tmpl to include additional server information
    
    * chore: Update sap_system module to use comma-separated database server VM resource IDs
    
    * chore: Update sap-vm-resources.tmpl to include additional server information
    
    * chore: Update sap_system module to include empty lists for SCS, application, and webdisp server VMs
    
    * chore: Update sap_system module to include application server VM resource IDs
    
    * chore: Refactor cluster_group_location task in ACSS registration role
    
    * Refactor cluster_group_location task in ACSS registration role
    
    * Refactor cluster_group_location task in ACSS registration role
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' instead of 'ansible.builtin.shell' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor cluster_group_location task in ACSS registration role
    
    * Refactor cluster_group_location task in ACSS registration role
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' instead of 'ansible.builtin.shell' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' instead of 'ansible.builtin.shell' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' instead of 'ansible.builtin.shell' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' for executing Azure CLI commands
    
    * Refactor ACSS registration role to use 'ansible.builtin.command' instead of 'ansible.builtin.shell' for executing Azure CLI commands
    
    * Refactor use_spn variable to default to false for all SAP deployment modules
    
    * Refactor systemd process limit configuration for pacemaker cluster
    
    * Refactor systemd process limit configuration for pacemaker cluster

--- deploy/terraform/run/sap_system/module.tf
+++ deploy/terraform/run/sap_system/module.tf
@@ -396,6 +396,7 @@ module "output_files" {
   scs_instance_number                           = var.scs_instance_number
   scs_server_loadbalancer_ip                    = module.app_tier.scs_server_loadbalancer_ip
   scs_server_ips                                = module.app_tier.scs_server_ips
+  scs_server_vm_resource_ids                    = module.app_tier.scs_vm_ids
   scs_server_secondary_ips                      = module.app_tier.scs_server_secondary_ips
   scs_vm_names                                  = module.app_tier.scs_vm_names
   use_local_credentials                         = module.common_infrastructure.use_local_credentials
