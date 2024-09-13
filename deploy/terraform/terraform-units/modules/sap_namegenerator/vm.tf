b84858bd2c78344cc28009c3214d326a2c0cb92e
Kimmo Forss <kimforss@microsoft.com>
2024-09-13 13:21:30 +0300

    Refactor HANA computer names and secondary DNS names for scale-out databases

--- deploy/terraform/terraform-units/modules/sap_namegenerator/vm.tf
+++ deploy/terraform/terraform-units/modules/sap_namegenerator/vm.tf
@@ -59,11 +59,11 @@ locals {
     format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 0, substr(local.random_id_vm_verified, 0, 2))
   ]
 
-  hana_computer_names_ha = [for idx in range(var.db_server_count) :
-    flatten(concat(
+  hana_computer_names_ha = flatten([for idx in range(var.db_server_count) :
+    concat(
       format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 0, substr(local.random_id_vm_verified, 0, 2)),
-      format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 1, substr(local.random_id_vm_verified, 0, 2))))
-  ]
+      format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 1, substr(local.random_id_vm_verified, 0, 2)))
+  ])
 
   hana_server_vm_names = [for idx in range(var.db_server_count) :
     length(var.db_zones) > 0 && var.use_zonal_markers ? (
@@ -72,18 +72,19 @@ locals {
     )
   ]
 
-  hana_server_vm_names_ha = [for idx in range(var.db_server_count) :
+  hana_server_vm_names_ha = flatten([for idx in range(var.db_server_count) :
     length(var.db_zones) > 0 && var.use_zonal_markers ? (
-      flatten(concat(
+      concat(
         format("%sd%s%sz%s%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), local.separator, var.db_zones[idx % max(length(var.db_zones), 1)], local.separator, idx + var.resource_offset, 0, local.random_id_vm_verified),
-      format("%sd%s%sz%s%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), local.separator, local.ha_zones[idx % max(length(local.ha_zones), 1)], local.separator, idx + var.resource_offset, 1, local.random_id_vm_verified)))) : (
-      flatten(concat(
+      format("%sd%s%sz%s%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), local.separator, local.ha_zones[idx % max(length(local.ha_zones), 1)], local.separator, idx + var.resource_offset, 1, local.random_id_vm_verified))) : (
+      concat(
         format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 0, local.random_id_vm_verified),
-        format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 1, local.random_id_vm_verified))
+        format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 1, local.random_id_vm_verified)
       )
 
     )
   ]
+  )
 
   scs_computer_names = [for idx in range(var.scs_server_count) :
     format("%sscs%02d%s%s", lower(var.sap_sid), idx + var.resource_offset, local.app_oscode, local.random_id_vm_verified)
@@ -139,11 +140,11 @@ locals {
     format("v%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 0, substr(local.random_id_vm_verified, 0, 2))
   ]
 
-  hana_secondary_dnsnames_ha = [for idx in range(var.db_server_count) :
-    flatten(concat(
+  hana_secondary_dnsnames_ha = flatten([for idx in range(var.db_server_count) :
+    concat(
       format("v%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 0, substr(local.random_id_vm_verified, 0, 2)),
-      format("v%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 1, local.random_id_virt_vm_verified)))
-  ]
+      format("v%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx + var.resource_offset, 1, local.random_id_virt_vm_verified))
+  ])
 
   scs_secondary_dnsnames = [for idx in range(var.scs_server_count) :
     format("v%ss%02d%s%s", lower(var.sap_sid), idx + var.resource_offset, local.app_oscode, local.random_id_virt_vm_verified)
