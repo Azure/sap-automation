45c6b6f8cd9807a3e26002ac098014d97de36457
Dhruv Aggarwal <dhaggarwal@microsoft.com>
2024-09-15 15:40:54 -0400

    testing

--- deploy/ansible/roles-sap-os/2.6-sap-mounts/library/setting_vars.py
+++ deploy/ansible/roles-sap-os/2.6-sap-mounts/library/setting_vars.py
@@ -37,13 +37,10 @@ def run_module():
             result['all_sap_mounts'] = module.params['multi_sids']
         
         else:
-            print("HELLLOOOOOOOO")
-            result['all_sap_mounts'].update(result['this_sid'])
+            result['all_sap_mounts'] += (result['this_sid'])
 
     except Exception as e:
         module.fail_json(msg=str(e),**result)
-
-    print(result['all_sap_mounts'])
 
     result['first_server_temp'].append(module.params['server_name'])
 
