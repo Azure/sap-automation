e3396166ad79b96c97fcc35bdc2551bc99e5139f
Kimmo Forss <kimforss@microsoft.com>
2024-10-10 22:12:02 +0300

    Refactor DNS settings to use environment variables for DNS resource group and subscription ID

--- deploy/terraform/terraform-units/modules/sap_landscape/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/infrastructure.tf
@@ -241,7 +241,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "storage" {
                                          )
 
   resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
-  private_dns_zone_name                = var.dns_zone_names.blob_dns_zone_name
+  private_dns_zone_name                = var.dns_settings.dns_zone_names.blob_dns_zone_name
   virtual_network_id                   = azurerm_virtual_network.vnet_sap[0].id
 }
 
