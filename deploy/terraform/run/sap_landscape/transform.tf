b065c89b81be1a31260aa1324b72fdf730501539
Kimmo Forss <kimforss@microsoft.com>
2024-10-10 20:30:36 +0300

    Refactor transform.tf to use environment variables for DNS resource group and subscription ID

--- deploy/terraform/run/sap_landscape/transform.tf
+++ deploy/terraform/run/sap_landscape/transform.tf
@@ -636,17 +636,12 @@ locals {
                                            dns_zone_names                               = var.dns_zone_names
                                            dns_server_list                              = var.dns_server_list
 
-                                           management_dns_resourcegroup_name            = coalesce(var.management_dns_resourcegroup_name, local.saplib_resource_group_name)
-                                           management_dns_subscription_id               = coalesce(var.management_dns_subscription_id, local.saplib_subscription_id)
+                                           management_dns_resourcegroup_name            = length(trimspace(var.management_dns_resourcegroup_name)) > 0 ? var.management_dns_resourcegroup_name : local.saplib_resource_group_name
+                                           management_dns_subscription_id               = length(trimspace(var.management_dns_subscription_id)) > 0 ? var.management_dns_subscription_id : local.saplib_subscription_id
 
-                                           privatelink_dns_subscription_id              = coalesce(var.privatelink_dns_subscription_id, var.management_dns_subscription_id, local.saplib_subscription_id)
-                                           privatelink_dns_resourcegroup_name           = coalesce(var.privatelink_dns_resourcegroup_name, var.management_dns_resourcegroup_name, local.saplib_resource_group_name)
+                                           privatelink_dns_resourcegroup_name           = length(trimspace(var.privatelink_dns_resourcegroup_name)) > 0 ? var.privatelink_dns_resourcegroup_name : length(trimspace(var.management_dns_resourcegroup_name)) > 0 ? var.management_dns_resourcegroup_name : local.saplib_resource_group_name
+                                           privatelink_dns_subscription_id              = length(trimspace(var.privatelink_dns_subscription_id)) > 0 ? var.privatelink_dns_subscription_id : (length(trimspace(var.management_dns_subscription_id)) > 0 ? var.management_dns_subscription_id : local.saplib_subscription_id)
 
-                                          #  management_dns_resourcegroup_name = trimspace(var.management_dns_resourcegroup_name)
-                                          #  management_dns_subscription_id = var.management_dns_subscription_id
-
-                                          #  privatelink_dns_subscription_id = var.privatelink_dns_subscription_id != var.management_dns_subscription_id ? var.privatelink_dns_subscription_id : var.management_dns_subscription_id
-                                          #  privatelink_dns_resourcegroup_name = var.management_dns_resourcegroup_name != var.privatelink_dns_resourcegroup_name ? var.privatelink_dns_resourcegroup_name : var.management_dns_resourcegroup_name
                                            register_storage_accounts_keyvaults_with_dns = var.register_storage_accounts_keyvaults_with_dns
                                            register_endpoints_with_dns                  = var.register_endpoints_with_dns
                                            register_virtual_network_to_dns              = var.register_virtual_network_to_dns
