1e2a849cf67423d5294edf1c5374e64b8e02f898
Kimmo Forss <kimforss@microsoft.com>
2024-10-10 21:07:04 +0300

    Refactor variables_local.tf to use environment variables for DNS resource group and subscription ID

--- deploy/terraform/run/sap_landscape/variables_local.tf
+++ deploy/terraform/run/sap_landscape/variables_local.tf
@@ -72,10 +72,10 @@ locals {
                                            var.management_dns_resourcegroup_name != (local.saplib_resource_group_name)
                                          )
 
-  management_dns_resourcegroup_name    =  coalesce(var.management_dns_resourcegroup_name, local.saplib_resource_group_name)
-  management_dns_subscription_id       =  coalesce(var.management_dns_subscription_id, local.saplib_subscription_id)
+  management_dns_resourcegroup_name    =  coalesce(var.management_dns_resourcegroup_name, local.saplib_resource_group_name, "11")
+  management_dns_subscription_id       =  coalesce(var.management_dns_subscription_id, local.saplib_subscription_id, "22")
 
-  privatelink_dns_resourcegroup_name   = coalesce(var.privatelink_dns_resourcegroup_name, var.management_dns_resourcegroup_name, local.saplib_resource_group_name)
-  privatelink_dns_subscription_id      = coalesce(var.privatelink_dns_subscription_id, var.management_dns_subscription_id, local.saplib_subscription_id)
+  privatelink_dns_resourcegroup_name   = coalesce(var.privatelink_dns_resourcegroup_name, var.management_dns_resourcegroup_name, local.saplib_resource_group_name, "33")
+  privatelink_dns_subscription_id      = coalesce(var.privatelink_dns_subscription_id, var.management_dns_subscription_id, local.saplib_subscription_id, "44")
 
 }
