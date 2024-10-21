bf1878f7f6e676b5b97e7f18a4fd221b55093377
Kimmo Forss <kimforss@microsoft.com>
2024-10-21 13:20:01 +0300

    Refactor key vault secrets to include service principal access

--- deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
@@ -167,7 +167,8 @@ resource "azurerm_key_vault_secret" "sid_ppk" {
                                            azurerm_key_vault_access_policy.kv_user,
                                            azurerm_role_assignment.role_assignment_spn,
                                            azurerm_role_assignment.role_assignment_msi,
-                                           azurerm_key_vault_access_policy.kv_user_msi
+                                           azurerm_key_vault_access_policy.kv_user_msi,
+                                           azurerm_key_vault_access_policy.kv_user_spn
                                          ]
   content_type                          = ""
   name                                  = local.sid_ppk_name
@@ -193,7 +194,8 @@ resource "azurerm_key_vault_secret" "sid_pk" {
                                            azurerm_key_vault_access_policy.kv_user,
                                            azurerm_role_assignment.role_assignment_spn,
                                            azurerm_role_assignment.role_assignment_msi,
-                                           azurerm_key_vault_access_policy.kv_user_msi
+                                           azurerm_key_vault_access_policy.kv_user_msi,
+                                           azurerm_key_vault_access_policy.kv_user_spn
                                          ]
   content_type                         = ""
   name                                 = local.sid_pk_name
@@ -224,7 +226,8 @@ resource "azurerm_key_vault_secret" "sid_username" {
                                           azurerm_key_vault_access_policy.kv_user,
                                           azurerm_role_assignment.role_assignment_spn,
                                            azurerm_role_assignment.role_assignment_msi,
-                                           azurerm_key_vault_access_policy.kv_user_msi
+                                           azurerm_key_vault_access_policy.kv_user_msi,
+                                           azurerm_key_vault_access_policy.kv_user_spn
                                         ]
   content_type                         = ""
   name                                 = local.sid_username_secret_name
@@ -253,7 +256,8 @@ resource "azurerm_key_vault_secret" "sid_password" {
                                            azurerm_key_vault_access_policy.kv_user,
                                            azurerm_role_assignment.role_assignment_spn,
                                            azurerm_role_assignment.role_assignment_msi,
-                                           azurerm_key_vault_access_policy.kv_user_msi
+                                           azurerm_key_vault_access_policy.kv_user_msi,
+                                           azurerm_key_vault_access_policy.kv_user_spn
                                          ]
   name                                 = local.sid_password_secret_name
   content_type                         = ""
@@ -284,7 +288,8 @@ resource "azurerm_key_vault_secret" "witness_access_key" {
                                            azurerm_key_vault_access_policy.kv_user,
                                            azurerm_role_assignment.role_assignment_spn,
                                            azurerm_role_assignment.role_assignment_msi,
-                                           azurerm_key_vault_access_policy.kv_user_msi
+                                           azurerm_key_vault_access_policy.kv_user_msi,
+                                           azurerm_key_vault_access_policy.kv_user_spn
                                          ]
   content_type                         = ""
   name                                 = replace(
@@ -321,7 +326,8 @@ resource "azurerm_key_vault_secret" "witness_name" {
                                            azurerm_key_vault_access_policy.kv_user,
                                            azurerm_role_assignment.role_assignment_spn,
                                            azurerm_role_assignment.role_assignment_msi,
-                                           azurerm_key_vault_access_policy.kv_user_msi
+                                           azurerm_key_vault_access_policy.kv_user_msi,
+                                           azurerm_key_vault_access_policy.kv_user_spn
                                          ]
   content_type                         = ""
   name                                 = replace(
@@ -388,7 +394,8 @@ resource "azurerm_key_vault_secret" "deployer_keyvault_user_name" {
                                            azurerm_key_vault_access_policy.kv_user,
                                            azurerm_role_assignment.role_assignment_spn,
                                            azurerm_role_assignment.role_assignment_msi,
-                                           azurerm_key_vault_access_policy.kv_user_msi
+                                           azurerm_key_vault_access_policy.kv_user_msi,
+                                           azurerm_key_vault_access_policy.kv_user_spn
                                          ]
   content_type                         = ""
   name                                 = "deployer-kv-name"
