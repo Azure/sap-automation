2346842f8fa75c9ec7fbf2a04a0001bd3474b963
Kimmo Forss <kimforss@microsoft.com>
2024-10-22 18:25:21 +0300

    Refactor deploy_controlplane.sh to improve parameter handling

--- deploy/scripts/deploy_controlplane.sh
+++ deploy/scripts/deploy_controlplane.sh
@@ -313,7 +313,7 @@ if [ 0 == $step ]; then
     echo "Calling install_deployer.sh:         $allParams"
     echo "Deployer State File:                 ${deployer_tfstate_key}"
 
-    "${SAP_AUTOMATION_REPO_PATH}"/deploy/scripts/install_deployer.sh
+    "${SAP_AUTOMATION_REPO_PATH}/deploy/scripts/install_deployer.sh" $allParams
     return_code=$?
     if [ 0 != $return_code ]; then
         echo "Bootstrapping of the deployer failed" > "${deployer_config_information}".err
