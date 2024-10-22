412d8661d8aee1effb663204d9f4ca6860143016
Kimmo Forss <kimforss@microsoft.com>
2024-10-22 14:48:20 +0300

    Refactor echo statements for improved formatting in installer.sh

--- deploy/scripts/deploy_controlplane.sh
+++ deploy/scripts/deploy_controlplane.sh
@@ -634,7 +634,7 @@ if [ 3 == $step ]; then
 
     allParams=$(printf " --parameterfile %s --storageaccountname %s --type sap_deployer %s %s " "${deployer_file_parametername}" "${REMOTE_STATE_SA}" "${approveparam}" "${ado_flag}" )
 
-    echo -e "$cyan calling installer.sh with parameters: $allParams $reset"
+    echo  "Calling installer.sh with parameters: $allParams"
 
     "${SAP_AUTOMATION_REPO_PATH}"/deploy/scripts/installer.sh $allParams
     return_code=$?
