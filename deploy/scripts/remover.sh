f0cac1e4c3d9fab571792b26a69b82fb4894bc47
Kimmo Forss <kimforss@microsoft.com>
2024-10-23 14:42:48 +0300

    Refactor echo statement in deploy pipeline to include return code from deployment

--- deploy/scripts/remover.sh
+++ deploy/scripts/remover.sh
@@ -56,7 +56,7 @@ function showhelp {
     echo "#   Example:                                                                            #"
     echo "#                                                                                       #"
     echo "#   [REPO-ROOT]deploy/scripts/remover.sh \                                              #"
-    echo "#      --parameterfile DEV-WEEU-SAP01-X00.json \                                        #"
+    echo "#      --parameterfile DEV-WEEU-SAP01-X00.tfvars \                                      #"
     echo "#      --type sap_system                                                                #"
     echo "#                                                                                       #"
     echo "#########################################################################################"
