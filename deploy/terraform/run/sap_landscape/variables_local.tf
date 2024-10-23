f32d3122c9b26f0acbc84e1cccc243e2156e1d59
Kimmo Forss <kimforss@microsoft.com>
2024-10-23 14:19:22 +0300

    Refactor echo statement in deploy pipeline to include return code from deployment

--- deploy/terraform/run/sap_landscape/variables_local.tf
+++ deploy/terraform/run/sap_landscape/variables_local.tf
@@ -29,10 +29,9 @@ locals {
 
   deployer_subscription_id             = coalesce(
                                            try(data.terraform_remote_state.deployer[0].outputs.created_resource_group_subscription_id,""),
-                                           length(local.spn_key_vault_arm_id) > 0 ? (
-                                             split("/", local.spn_key_vault_arm_id)[2]) : (
-                                             ""
-                                         ))
+                                           length(local.spn_key_vault_arm_id) > 0 ? (split("/", local.spn_key_vault_arm_id)[2]) : (""),
+                                           local.saplib_subscription_id
+                                           )
 
   spn                                  = {
                                            subscription_id = data.azurerm_key_vault_secret.subscription_id.value,
