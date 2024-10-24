7aac0624dea8d43ae5fa9fb9840a35605d5e8679
Kimmo Forss <kimforss@microsoft.com>
2024-10-24 10:18:49 +0300

    Refactor variables_local.tf to improve readability and error handling

--- deploy/terraform/run/sap_landscape/variables_local.tf
+++ deploy/terraform/run/sap_landscape/variables_local.tf
@@ -23,7 +23,7 @@ locals {
 
   // Retrieve the arm_id of deployer's Key Vault from deployer's terraform.tfstate
   spn_key_vault_arm_id                 = coalesce(
-                                           local.key_vault.kv_spn_id,
+                                           try(local.key_vault.kv_spn_id,""),
                                            try(data.terraform_remote_state.deployer[0].outputs.deployer_kv_user_arm_id, "")
                                          )
 
