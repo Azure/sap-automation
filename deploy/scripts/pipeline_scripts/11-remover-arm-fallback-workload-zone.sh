91cee5974757c98c84b98597764ca2125d392825
Kimmo Forss <kimforss@microsoft.com>
2024-11-28 11:09:08 +0200

    Pipeline updates to ease debugging. Instead of using inline scripts the scripts are now in the file system

---
+++ deploy/scripts/pipeline_scripts/11-remover-arm-fallback-workload-zone.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+green="\e[1;32m"
+reset="\e[0m"
+echo -e "$green--- Configure devops CLI extension ---$reset"
+az config set extension.use_dynamic_install=yes_without_prompt --only-show-errors --output none
+
+if [ "$SYSTEM_DEBUG" = True ]; then
+  set -x
+  debug=true
+  export debug
+fi
+
+if [ "${#PAT}" -gt 0 ]; then
+  echo "Using PAT for authentication"
+  AZURE_DEVOPS_EXT_PAT=$PAT
+else
+  echo "Using SYSTEM_ACCESSTOKEN for authentication"
+  AZURE_DEVOPS_EXT_PAT=$SYSTEM_ACCESSTOKEN
+fi
+
+export AZURE_DEVOPS_EXT_PAT
+
+az devops configure --defaults organization="$SYSTEM_COLLECTIONURI" project="$SYSTEM_TEAMPROJECT" --output none
+
+return_code=0
+
+echo -e "$green--- Adding deployment automation configuration to devops repository ---$reset"
+ENVIRONMENT=$(echo "$WORKLOAD_ZONE_FOLDERNAME" | awk -F'-' '{print $1}' | xargs)
+echo "Environment:                           $ENVIRONMENT"
+
+LOCATION=$(echo "$WORKLOAD_ZONE_FOLDERNAME" | awk -F'-' '{print $2}' | xargs)
+echo
+echo "Location:                              $LOCATION"
+
+NETWORK=$(echo "$WORKLOAD_ZONE_FOLDERNAME" | awk -F'-' '{print $3}' | xargs)
+echo "Network:                               $NETWORK"
+
+cd "$CONFIG_REPO_PATH" || exit
+git config --global user.email "$BUILD_REQUESTEDFOREMAIL"
+git config --global user.name "$BUILD_REQUESTEDFOR"
+git commit -m "Added updates from devops deployment $BUILD_BUILDNUMBER [skip ci]"
+
+git checkout -q "$BRANCH"
+git clean -d -f -X
+
+
+echo "##vso[build.updatebuildnumber]Removing workload zone $WORKLOAD_ZONE_FOLDERNAME"
+changed=0
+
+if [ -d "LANDSCAPE/$WORKLOAD_ZONE_FOLDERNAME/.terraform" ]; then
+  git rm -r -f --ignore-unmatch "LANDSCAPE/$WORKLOAD_ZONE_FOLDERNAME/.terraform"
+  changed=1
+fi
+if [ -f ".sap_deployment_automation/${ENVIRONMENT}${LOCATION}${NETWORK}" ]; then
+  git rm --ignore-unmatch -f ".sap_deployment_automation/${ENVIRONMENT}${LOCATION}${NETWORK}"
+  changed=1
+fi
+if [ -f ".sap_deployment_automation/${ENVIRONMENT}${LOCATION}${NETWORK}.md" ]; then
+  git rm --ignore-unmatch -f ".sap_deployment_automation/${ENVIRONMENT}${LOCATION}${NETWORK}.md"
+  changed=1
+fi
+
+if [ 1 == $changed ]; then
+  git commit -m "Removal of Workload zone $BUILD_BUILDNUMBER [skip ci]"
+  if git -c http.extraheader="AUTHORIZATION: bearer $SYSTEM_ACCESSTOKEN" push; then
+    echo "Removal of Workload zone $BUILD_BUILDNUMBER pushed to devops repository"
+  else
+    echo "Failed to push changes to devops repository"
+    return_code=1
+  fi
+fi
+echo -e "$green--- Configure devops CLI extension ---$reset"
+az config set extension.use_dynamic_install=yes_without_prompt
+
+VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$VARIABLE_GROUP'].id | [0]")
+
+prefix="${ENVIRONMENT}${LOCATION}${NETWORK}"
+
+echo "Variable Group:                        $VARIABLE_GROUP_ID"
+
+if [ -n "$VARIABLE_GROUP_ID" ]; then
+  echo "Deleting variables"
+  if [ -n "$(Terraform_Remote_Storage_Account_Name)" ]; then
+    az pipelines variable-group variable delete --group-id "${VARIABLE_GROUP_ID}" --name Terraform_Remote_Storage_Account_Name --yes --only-show-errors
+  fi
+
+  if [ -n "$(Terraform_Remote_Storage_Subscription)" ]; then
+    az pipelines variable-group variable delete --group-id "${VARIABLE_GROUP_ID}" --name Terraform_Remote_Storage_Subscription --yes --only-show-errors >/dev/null 2>&1
+  fi
+
+  if [ -n "$(Deployer_State_FileName)" ]; then
+    az pipelines variable-group variable delete --group-id "${VARIABLE_GROUP_ID}" --name Deployer_State_FileName --yes --only-show-errors >/dev/null 2>&1
+  fi
+
+  if [ -n "$(Deployer_Key_Vault)" ]; then
+    az pipelines variable-group variable delete --group-id "${VARIABLE_GROUP_ID}" --name Deployer_Key_Vault --yes --only-show-errors >/dev/null 2>&1
+  fi
+
+  az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "${prefix}Workload_Key_Vault.value")
+  if [ -n "${az_var}" ]; then
+    az pipelines variable-group variable delete --group-id "${VARIABLE_GROUP_ID}" --name "${prefix}Workload_Key_Vault" --yes --only-show-errors
+  fi
+
+  az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "${prefix}Workload_Zone_State_FileName.value")
+  if [ -n "${az_var}" ]; then
+    az pipelines variable-group variable delete --group-id "${VARIABLE_GROUP_ID}" --name "${prefix}Workload_Zone_State_FileName" --yes --only-show-errors
+  fi
+
+  az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "${prefix}Workload_Secret_Prefix.value")
+  if [ -n "${az_var}" ]; then
+    az pipelines variable-group variable delete --group-id "${VARIABLE_GROUP_ID}" --name "${prefix}Workload_Secret_Prefix" --yes --only-show-errors
+  fi
+fi
+
+exit $return_code
---
+++ deploy/scripts/pipeline_scripts/12-remove-control-plane.sh
@@ -0,0 +1,302 @@
+#!/bin/bash
+
+echo "##vso[build.updatebuildnumber]Removing the control plane defined in $DEPLOYER_FOLDERNAME $LIBRARY_FOLDERNAME"
+green="\e[1;32m"
+reset="\e[0m"
+bold_red="\e[1;31m"
+cyan="\e[1;36m"
+
+# External helper functions
+#. "$(dirname "${BASH_SOURCE[0]}")/deploy_utils.sh"
+full_script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_directory="$(dirname "${full_script_path}")"
+
+#call stack has full scriptname when using source
+source "${script_directory}/helper.sh"
+
+DEBUG=False
+
+if [ "$SYSTEM_DEBUG" = True ]; then
+  set -x
+  set -eu
+  DEBUG=True
+fi
+
+export DEBUG
+# Ensure that the exit status of a pipeline command is non-zero if any
+# stage of the pipefile has a non-zero exit status.
+set -o pipefail
+
+deployerTFvarsFile="${CONFIG_REPO_PATH}/DEPLOYER/$DEPLOYER_FOLDERNAME/$DEPLOYER_TFVARS_FILENAME"
+libraryTFvarsFile="${CONFIG_REPO_PATH}/LIBRARY/$LIBRARY_FOLDERNAME/$LIBRARY_TFVARS_FILENAME"
+deployer_tfstate_key="$DEPLOYER_FOLDERNAME.terraform.tfstate"
+
+echo -e "$green--- File Validations ---$reset"
+
+if [ ! -f "$deployerTFvarsFile" ]; then
+  echo -e "$bold_red--- File ${deployerTFvarsFile} was not found ---$reset"
+  echo "##vso[task.logissue type=error]File DEPLOYER/$DEPLOYER_FOLDERNAME/$DEPLOYER_TFVARS_FILENAME was not found."
+  exit 2
+fi
+
+if [ ! -f "${libraryTFvarsFile}" ]; then
+  echo -e "$bold_red--- File ${libraryTFvarsFile}  was not found ---$reset"
+  echo "##vso[task.logissue type=error]File LIBRARY/$LIBRARY_FOLDERNAME/$LIBRARY_TFVARS_FILENAME was not found."
+  exit 2
+fi
+
+TF_VAR_deployer_tfstate_key="$deployer_tfstate_key"
+export TF_VAR_deployer_tfstate_key
+
+echo -e "$green--- Environment information ---$reset"
+ENVIRONMENT=$(grep -m1 "^environment" "$deployerTFvarsFile" | awk -F'=' '{print $2}' | tr -d ' \t\n\r\f"' || true)
+LOCATION=$(grep -m1 "^location" "$deployerTFvarsFile" | awk -F'=' '{print $2}' | tr '[:upper:]' '[:lower:]' | tr -d ' \t\n\r\f"' || true)
+
+deployer_environment_file_name="${CONFIG_REPO_PATH}/.sap_deployment_automation/${ENVIRONMENT}$LOCATION"
+
+# shellcheck disable=SC2005
+ENVIRONMENT_IN_FILENAME=$(echo $DEPLOYER_FOLDERNAME | awk -F'-' '{print $1}')
+
+LOCATION_CODE_IN_FILENAME=$(echo $DEPLOYER_FOLDERNAME | awk -F'-' '{print $2}')
+LOCATION_IN_FILENAME=$(get_region_from_code "$LOCATION_CODE_IN_FILENAME" || true)
+
+echo "Environment:                         ${ENVIRONMENT}"
+echo "Location:                            ${LOCATION}"
+echo "Environment(filename):               $ENVIRONMENT_IN_FILENAME"
+echo "Location(filename):                  $LOCATION_IN_FILENAME"
+echo ""
+echo "Agent:                               $THIS_AGENT"
+echo "Organization:                        $SYSTEM_COLLECTIONURI"
+echo "Project:                             $SYSTEM_TEAMPROJECT"
+
+if [ "$ENVIRONMENT" != "$ENVIRONMENT_IN_FILENAME" ]; then
+  echo "##vso[task.logissue type=error]The environment setting in $deployerTFvarsFile $ENVIRONMENT does not match the $DEPLOYER_FOLDERNAME file name $ENVIRONMENT_IN_FILENAME. Filename should have the pattern [ENVIRONMENT]-[REGION_CODE]-[NETWORK_LOGICAL_NAME]-INFRASTRUCTURE"
+  exit 2
+fi
+
+if [ "$LOCATION" != "$LOCATION_IN_FILENAME" ]; then
+  echo "##vso[task.logissue type=error]The location setting in $deployerTFvarsFile $LOCATION does not match the $DEPLOYER_FOLDERNAME file name $LOCATION_IN_FILENAME. Filename should have the pattern [ENVIRONMENT]-[REGION_CODE]-[NETWORK_LOGICAL_NAME]-INFRASTRUCTURE"
+  exit 2
+fi
+
+deployer_environment_file_name="$CONFIG_REPO_PATH/.sap_deployment_automation/$ENVIRONMENT$LOCATION_CODE_IN_FILENAME"
+echo "Environment file:                    $deployer_environment_file_name"
+
+REMOTE_STATE_SA=""
+REMOTE_STATE_RG=$LIBRARY_FOLDERNAME
+
+echo -e "$green--- Configure devops CLI extension ---$reset"
+
+echo "Using SYSTEM_ACCESSTOKEN for authentication"
+AZURE_DEVOPS_EXT_PAT=$SYSTEM_ACCESSTOKEN
+
+export AZURE_DEVOPS_EXT_PAT
+
+az config set extension.use_dynamic_install=yes_without_prompt --only-show-errors
+az extension add --name azure-devops --output none --only-show-errors
+az devops configure --defaults organization="$SYSTEM_COLLECTIONURI" project="$SYSTEM_TEAMPROJECT" --output none --only-show-errors
+
+if [[ -f /etc/profile.d/deploy_server.sh ]]; then
+  path=$(grep -m 1 "export PATH=" /etc/profile.d/deploy_server.sh | awk -F'=' '{print $2}' | xargs)
+  export PATH=$PATH:$path
+fi
+
+echo -e "$green--- Information ---$reset"
+VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$PARENT_VARIABLE_GROUP'].id | [0]")
+
+if [ -z "${VARIABLE_GROUP_ID}" ]; then
+  echo "##vso[task.logissue type=error]Variable group $PARENT_VARIABLE_GROUP could not be found."
+  exit 2
+fi
+
+if [ -z "$ARM_SUBSCRIPTION_ID" ]; then
+  echo "##vso[task.logissue type=error]Variable ARM_SUBSCRIPTION_ID was not defined."
+  exit 2
+fi
+
+echo -e "$green--- Validations ---$reset"
+if [ "$USE_MSI" != "true" ]; then
+
+  if [ -z "$CP_ARM_CLIENT_ID" ]; then
+    echo "##vso[task.logissue type=error]Variable CP_ARM_CLIENT_ID was not defined in the $(variable_group) variable group."
+    exit 2
+  fi
+
+  if [ "$CP_ARM_CLIENT_ID" == '$$(CP_ARM_CLIENT_ID)' ]; then
+    echo "##vso[task.logissue type=error]Variable ARM_CLIENT_ID was not defined in the $(variable_group) variable group."
+    exit 2
+  fi
+
+  if [ -z "$CP_ARM_CLIENT_SECRET" ]; then
+    echo "##vso[task.logissue type=error]Variable CP_ARM_CLIENT_SECRET was not defined in the $(variable_group) variable group."
+    exit 2
+  fi
+
+  if [ "$CP_ARM_CLIENT_SECRET" == '$$(CP_ARM_CLIENT_SECRET)' ]; then
+    echo "##vso[task.logissue type=error]Variable ARM_CLIENT_SECRET was not defined in the $(variable_group) variable group."
+    exit 2
+  fi
+
+  if [ -z "$CP_ARM_TENANT_ID" ]; then
+    echo "##vso[task.logissue type=error]Variable CP_ARM_TENANT_ID was not defined in the $(variable_group) variable group."
+    exit 2
+  fi
+
+  if [ "$CP_WL_ARM_TENANT_ID" == '$$(CP_ARM_TENANT_ID)' ]; then
+    echo "##vso[task.logissue type=error]Variable CP_ARM_TENANT_ID was not defined in the $(variable_group) variable group."
+    exit 2
+  fi
+
+fi
+
+if [ "$USE_MSI" != "true" ]; then
+  # Set logon variables
+  ARM_CLIENT_ID="$CP_ARM_CLIENT_ID"
+  export ARM_CLIENT_ID
+  ARM_CLIENT_SECRET="$CP_ARM_CLIENT_SECRET"
+  export ARM_CLIENT_SECRET
+  ARM_TENANT_ID=$CP_ARM_TENANT_ID
+  export ARM_TENANT_ID
+fi
+ARM_SUBSCRIPTION_ID=$CP_ARM_SUBSCRIPTION_ID
+export ARM_SUBSCRIPTION_ID
+
+# Check if running on deployer
+if [[ ! -f /etc/profile.d/deploy_server.sh ]]; then
+  configureNonDeployer "$TF_VERSION" || true
+  echo -e "$green--- az login ---$reset"
+  LogonToAzure false || true
+else
+  LogonToAzure "$USE_MSI" || true
+fi
+return_code=$?
+if [ 0 != $return_code ]; then
+  echo -e "$bold_red--- Login failed ---$reset"
+  echo "##vso[task.logissue type=error]az login failed."
+  exit $return_code
+fi
+
+ARM_SUBSCRIPTION_ID=$CP_ARM_SUBSCRIPTION_ID
+export ARM_SUBSCRIPTION_ID
+az account set --subscription "$ARM_SUBSCRIPTION_ID"
+
+key_vault=$(getVariableFromVariableGroup "${VARIABLE_GROUP_ID}" "Deployer_Key_Vault" "${deployer_environment_file_name}" "keyvault" || true)
+export key_vault
+
+STATE_SUBSCRIPTION=$ARM_SUBSCRIPTION_ID
+export STATE_SUBSCRIPTION
+
+REMOTE_STATE_SA=$(getVariableFromVariableGroup "${VARIABLE_GROUP_ID}" "Terraform_Remote_Storage_Account_Name" "${deployer_environment_file_name}" "REMOTE_STATE_SA" || true)
+export REMOTE_STATE_SA
+
+REMOTE_STATE_RG=$(getVariableFromVariableGroup "${VARIABLE_GROUP_ID}" "Terraform_Remote_Storage_Resource_Group_Name" "${deployer_environment_file_name}" "REMOTE_STATE_SA" || true)
+export REMOTE_STATE_RG
+
+echo "Terraform state subscription:        $STATE_SUBSCRIPTION"
+echo "Terraform state rg name:             $REMOTE_STATE_RG"
+echo "Terraform state account:             $REMOTE_STATE_SA"
+echo "Deployer Key Vault:                  ${key_vault}"
+
+if [ -f "${CONFIG_REPO_PATH}/LIBRARY/$LIBRARY_FOLDERNAME/state.zip" ]; then
+  pass=${SYSTEM_COLLECTIONID//-/}
+  unzip -qq -o -P "${pass}" "${CONFIG_REPO_PATH}/LIBRARY/$LIBRARY_FOLDERNAME/state.zip" -d "${CONFIG_REPO_PATH}/LIBRARY/$LIBRARY_FOLDERNAME"
+fi
+
+if [ -f "${CONFIG_REPO_PATH}/DEPLOYER/$DEPLOYER_FOLDERNAME/state.zip" ]; then
+  pass=${SYSTEM_COLLECTIONID//-/}
+  unzip -qq -o -P "${pass}" "${CONFIG_REPO_PATH}/DEPLOYER/$DEPLOYER_FOLDERNAME/state.zip" -d "${CONFIG_REPO_PATH}/DEPLOYER/$DEPLOYER_FOLDERNAME"
+fi
+
+echo -e "$green--- Running the remove region script that destroys deployer VM and SAP library ---$reset"
+
+if "$SAP_AUTOMATION_REPO_PATH/deploy/scripts/remove_controlplane.sh" \
+  --deployer_parameter_file "$deployerTFvarsFile" \
+  --library_parameter_file "$libraryTFvarsFile" \
+  --storage_account "$REMOTE_STATE_SA" \
+  --subscription "${STATE_SUBSCRIPTION}" \
+  --resource_group "$REMOTE_STATE_RG" \
+  --ado --auto-approve --keep_agent; then
+  return_code=$?
+  echo "Control Plane $DEPLOYER_FOLDERNAME removal step 1 completed."
+  echo "##vso[task.logissue type=warning]Control Plane $DEPLOYER_FOLDERNAME removal step 1 completed."
+else
+  return_code=$?
+  echo "Control Plane $DEPLOYER_FOLDERNAME removal step 1 failed."
+fi
+return_code=$?
+
+echo "Return code from remove_controlplane: $return_code."
+
+echo -e "$green--- Remove Control Plane Part 1 ---$reset"
+cd "$CONFIG_REPO_PATH" || exit
+git checkout -q "$BRANCH"
+
+changed=0
+if [ -f "$deployer_environment_file_name" ]; then
+  git add "$deployer_environment_file_name"
+  changed=1
+fi
+
+if [ -f "DEPLOYER/$DEPLOYER_FOLDERNAME/.terraform/terraform.tfstate" ]; then
+  git add -f "DEPLOYER/$DEPLOYER_FOLDERNAME/.terraform/terraform.tfstate"
+  changed=1
+  local_backend=$(grep "\"type\": \"local\"" "DEPLOYER/$DEPLOYER_FOLDERNAME/.terraform/terraform.tfstate" || true)
+  if [ -n "${local_backend}" ]; then
+
+    if [ -f "DEPLOYER/$DEPLOYER_FOLDERNAME/terraform.tfstate" ]; then
+      echo "Compressing the state file."
+      sudo apt-get -qq install zip
+      pass=${SYSTEM_COLLECTIONID//-/}
+
+      if zip -q -j -P "${pass}" "DEPLOYER/$DEPLOYER_FOLDERNAME/state DEPLOYER/$DEPLOYER_FOLDERNAME/terraform.tfstate"; then
+        git add -f "DEPLOYER/$DEPLOYER_FOLDERNAME/state.zip"
+      fi
+    fi
+  fi
+fi
+
+if [ -f "LIBRARY/$LIBRARY_FOLDERNAME/.terraform/terraform.tfstate" ]; then
+  git add -f "LIBRARY/$LIBRARY_FOLDERNAME/.terraform/terraform.tfstate"
+  changed=1
+  local_backend=$(grep "\"type\": \"local\"" "LIBRARY/$LIBRARY_FOLDERNAME/.terraform/terraform.tfstate" || true)
+  if [ -n "${local_backend}" ]; then
+    echo "Local Terraform state"
+    if [ -f "LIBRARY/$LIBRARY_FOLDERNAME/terraform.tfstate" ]; then
+      sudo apt-get -qq install zip
+      echo "Compressing the library state file"
+      pass=${SYSTEM_COLLECTIONID//-/}
+      zip -q -j -P "${pass}" "LIBRARY/$LIBRARY_FOLDERNAME/state" "LIBRARY/$LIBRARY_FOLDERNAME/terraform.tfstate"
+      git add -f "LIBRARY/$LIBRARY_FOLDERNAME/state.zip"
+      changed=1
+    fi
+  else
+    echo "Remote Terraform state"
+    if [ -f "LIBRARY/$LIBRARY_FOLDERNAME/terraform.tfstate" ]; then
+      git rm -q -f --ignore-unmatch "LIBRARY/$LIBRARY_FOLDERNAME/terraform.tfstate"
+      changed=1
+    fi
+    if [ -f "LIBRARY/$LIBRARY_FOLDERNAME/state.zip" ]; then
+      git rm -q --ignore-unmatch -f "LIBRARY/$LIBRARY_FOLDERNAME/state.zip"
+      changed=1
+    fi
+  fi
+fi
+
+if [ 1 == $changed ]; then
+  git config --global user.email "$BUILD_REQUESTEDFOREMAIL"
+  git config --global user.name "$BUILD_REQUESTEDFOR"
+
+  if git commit -m "Control Plane $DEPLOYER_FOLDERNAME removal step 1[skip ci]"; then
+
+    if git -c http.extraheader="AUTHORIZATION: bearer $SYSTEM_ACCESSTOKEN" push --set-upstream origin "$BRANCH" --force-with-lease; then
+      return_code=$?
+      echo "##vso[task.logissue type=warning]Control Plane $DEPLOYER_FOLDERNAME removal step 2 updated in $BRANCH"
+    else
+      return_code=$?
+      echo "##vso[task.logissue type=error]Failed to push changes to $BRANCH"
+    fi
+  fi
+
+fi
+
+exit $return_code
