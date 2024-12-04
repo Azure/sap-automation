2fab47dc60a058eeefdbb1290b84613c64f42807
Dhruv Aggarwal <dhaggarwal@microsoft.com>
2024-12-04 10:21:21 +0530

    changed formatting and cleaned up file names

--- deploy/ansible/roles-sap-os/2.6-sap-mounts/library/config_sap_mounts.py
+++ deploy/ansible/roles-sap-os/2.6-sap-mounts/library/config_sap_mounts.py
@@ -1,10 +1,13 @@
 """Settings Vars Module for SAP Mounts Role. This uses the AnsibleModule from the Ansible module_utils to set the parameters for the SAP mounts.
 """
+
 from ansible.module_utils.basic import AnsibleModule
+
+
 def run_module():
     """This function sets the parameters for the SAP mounts.
-        Input parameters are the SAP SID, HDBADM UID, platform, SIDADM UID, multi SIDs, asesidadm_uid(not required), SCS instance number, PAS instance number, APP instance number, server name and distribution full ID(not required). 
-        The output parameters are the SID, all_sap_mounts, first_server_temp, mnt_options and nfs_service.
+    Input parameters are the SAP SID, HDBADM UID, platform, SIDADM UID, multi SIDs, asesidadm_uid(not required), SCS instance number, PAS instance number, APP instance number, server name and distribution full ID(not required).
+    The output parameters are the SID, all_sap_mounts, first_server_temp, mnt_options and nfs_service.
     """
     distro_versions = [
         "redhat8.4",
@@ -18,8 +21,8 @@ def run_module():
         "sles_sap15.5",
         "sles_sap15.4",  # sles stands for SUSE Linux Enterprise Server
         "sles_sap15.5",
-        ]
-    
+    ]
+
     module_args = dict(
         sap_sid=dict(type="str", required=True),
         hdbadm_uid=dict(type="str", required=True),
@@ -35,31 +38,16 @@ def run_module():
     )
 
     result = {
-    "this_sid": {},
-    "all_sap_mounts": [],
-    "first_server_temp": [],
-    "mnt_options": {},
-    "nfs_service": "",
+        "this_sid": {},
+        "all_sap_mounts": [],
+        "first_server_temp": [],
+        "mnt_options": {},
+        "nfs_service": "",
     }
 
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
 
     distribution_full_id = module.params["distribution_full_id"]
-
-    result["this_sid"] = {
-        "sid": module.params["sap_sid"].upper(),
-        "dbsid_uid": module.params["hdbadm_uid"],
-        "sidadm_uid": (
-            module.params["asesidadm_uid"]
-            if module.params["platform"] == "SYSBASE"
-            else module.params["sidadm_uid"]
-        ),
-        "ascs_inst_no": module.params["scs_instance_number"],
-        "pas_inst_no": module.params["pas_instance_number"],
-        "app_inst_no": module.params["app_instance_number"],
-    }
-
-    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
 
     result["this_sid"] = {
         "sid": module.params["sap_sid"].upper(),
@@ -87,7 +75,8 @@ def run_module():
 
     result["mnt_options"] = {
         "afs_mnt_options": "noresvport,vers=4,minorversion=1,sec=sys",
-        "anf_mnt_options": "rw,nfsvers=4.1,hard,timeo=600,rsize=262144,wsize=262144,noatime,lock,_netdev,sec=sys" + (",nconnect=8" if distribution_full_id in distro_versions else ""),
+        "anf_mnt_options": "rw,nfsvers=4.1,hard,timeo=600,rsize=262144,wsize=262144,noatime,lock,_netdev,sec=sys"
+        + (",nconnect=8" if distribution_full_id in distro_versions else ""),
     }
 
     nfs_service_mapping = {
@@ -96,9 +85,12 @@ def run_module():
         "redhat7": "nfs",
         "oraclelinux8": "rpcbind",
     }
-    result["nfs_service"] = nfs_service_mapping.get(distribution_full_id.split('.')[0], "nfs-server")
+    result["nfs_service"] = nfs_service_mapping.get(
+        distribution_full_id.split(".")[0], "nfs-server"
+    )
 
     module.exit_json(**result)
+
 
 if __name__ == "__main__":
     run_module()
--- deploy/ansible/roles-sap-os/2.6-sap-mounts/library/config_sap_mounts_oracle.py
+++ deploy/ansible/roles-sap-os/2.6-sap-mounts/library/config_sap_mounts_oracle.py
@@ -1,11 +1,13 @@
 """Settings Vars Module for SAP Mounts Oracle Role. This uses the AnsibleModule from the Ansible module_utils to set the parameters for the SAP mounts on Oracle.
 """
+
 from ansible.module_utils.basic import AnsibleModule
 
+
 def run_module():
-    """ This function sets the parameters for the SAP mounts on Oracle.
-        Input parameters are nfs_server and NFS_provider. 
-        The output parameters are nfs_server_temp and nfs_server.
+    """This function sets the parameters for the SAP mounts on Oracle.
+    Input parameters are nfs_server and NFS_provider.
+    The output parameters are nfs_server_temp and nfs_server.
     """
     module_args = dict(
         nfs_server_temp=dict(type="str", required=True),
@@ -22,6 +24,7 @@ def run_module():
     result["nfs_server_temp"].append(module.params["nfs_server_temp"])
 
     module.exit_json(**result)
+
 
 if __name__ == "__main__":
     run_module()
