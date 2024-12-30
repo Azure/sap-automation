4a30d11c595378bb24dd6b9678881230488801b3
Kimmo Forss <kimforss@microsoft.com>
2024-12-30 13:07:35 +0200

    Fix variable file reference in install_library.sh for custom_random_id handling

--- deploy/scripts/install_library.sh
+++ deploy/scripts/install_library.sh
@@ -564,7 +564,7 @@ if ! terraform -chdir="${terraform_module_directory}" output | grep "No outputs"
 	if [ -n "${library_random_id}" ]; then
 		save_config_var "library_random_id" "${library_config_information}"
 		custom_random_id="${library_random_id:0:3}"
-		sed -i -e /"custom_random_id"/d "${parameterfile}"
+		sed -i -e /"custom_random_id"/d "${var_file}"
 		printf "# The parameter 'custom_random_id' can be used to control the random 3 digits at the end of the storage accounts and key vaults\ncustom_random_id=\"%s\"\n" "${custom_random_id}" >>"${var_file}"
 
 	fi
