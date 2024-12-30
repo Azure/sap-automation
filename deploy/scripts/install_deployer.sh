10b0aad9cc75562d846f5f853d7ef3dd96d9a7c6
Kimmo Forss <kimforss@microsoft.com>
2024-12-30 13:48:07 +0200

    Fix variable file reference in install_deployer.sh for custom_random_id handling

--- deploy/scripts/install_deployer.sh
+++ deploy/scripts/install_deployer.sh
@@ -469,7 +469,7 @@ if ! terraform -chdir="${terraform_module_directory}" output | grep "No outputs"
 		if [ -n "${deployer_random_id}" ]; then
 			save_config_var "deployer_random_id" "${deployer_config_information}"
 			custom_random_id="${deployer_random_id:0:3}"
-			sed -i -e /"custom_random_id"/d "${parameterfile}"
+			sed -i -e /"custom_random_id"/d "${var_file}"
 			printf "# The parameter 'custom_random_id' can be used to control the random 3 digits at the end of the storage accounts and key vaults\ncustom_random_id=\"%s\"\n" "${custom_random_id}" >>"${var_file}"
 
 	fi
