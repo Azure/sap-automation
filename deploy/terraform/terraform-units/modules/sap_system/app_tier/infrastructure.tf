74722d3ea56750230934a98b46b6b2c317ee36d3
Kimmo Forss <kimforss@microsoft.com>
2024-12-31 11:57:33 +0200

    Fix proximity placement group ID assignment in infrastructure.tf to handle potential null values

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
@@ -339,7 +339,7 @@ resource "azurerm_availability_set" "app" {
   resource_group_name                  = var.resource_group[0].name
   platform_update_domain_count         = 20
   platform_fault_domain_count          = local.faultdomain_count
-  proximity_placement_group_id         = var.ppg[count.index]
+  proximity_placement_group_id         = try(var.ppg[count.index], null)
   managed                              = true
   tags                                 = var.tags
 }
