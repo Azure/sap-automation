c6e188975bb8aea2b7016ee37ebeddf04cd06a53
Kimmo Forss <kimforss@microsoft.com>
2025-04-12 22:25:14 +0300

    Refactor ARM_CLIENT_SECRET handling and introduce ARM_USE_MSI variable for clarity in SAP workload zone script

--- deploy/scripts/pipeline_scripts/02-sap-workload-zone.sh
+++ deploy/scripts/pipeline_scripts/02-sap-workload-zone.sh
@@ -113,9 +113,14 @@ if [ $USE_MSI != "true" ]; then
 	export ARM_CLIENT_SECRET
 	ARM_TENANT_ID=$CP_ARM_TENANT_ID
 	export ARM_TENANT_ID
+else
+	unset ARM_CLIENT_SECRET
+	ARM_USE_MSI=true
+	export ARM_USE_MSI
+
 fi
-	ARM_SUBSCRIPTION_ID=$CP_ARM_SUBSCRIPTION_ID
-	export ARM_SUBSCRIPTION_ID
+ARM_SUBSCRIPTION_ID=$CP_ARM_SUBSCRIPTION_ID
+export ARM_SUBSCRIPTION_ID
 # Check if running on deployer
 if [[ ! -f /etc/profile.d/deploy_server.sh ]]; then
 	configureNonDeployer "$TF_VERSION"
