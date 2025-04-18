6aef5364d18c46a277ab2e889d116a76ffc24df8
Kimmo Forss <kimforss@microsoft.com>
2025-04-18 14:12:53 +0300

    Refactor firewall_exists assignment to ensure proper type handling of firewall_id

--- deploy/terraform/terraform-units/modules/sap_landscape/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/variables_local.tf
@@ -27,7 +27,7 @@ locals {
   // Firewall
 
 
-  firewall_exists                                 = length(try(var.deployer_tfstate.firewall_id, "")) > 0
+  firewall_exists                                 = length(try(tostring(var.deployer_tfstate.firewall_id), "")) > 0
   firewall_id                                     = local.firewall_exists ? try(var.deployer_tfstate.firewall_id, "") : ""
   firewall_ip                                     = local.firewall_exists ? try(var.deployer_tfstate.firewall_ip, "") : ""
   firewall_name                                   = local.firewall_exists ? try(split("/", local.firewall_id)[8], "") : ""
