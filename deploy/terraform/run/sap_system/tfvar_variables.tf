89402ab4044d6941a18877b501bdabe813dd46cf
Kimmo Forss <kimforss@microsoft.com>
2025-06-04 18:03:42 +0300

    Remove duplicate encryption_at_host_enabled assignments in VM observer configurations

--- deploy/terraform/run/sap_system/tfvar_variables.tf
+++ deploy/terraform/run/sap_system/tfvar_variables.tf
@@ -150,12 +150,6 @@ variable "enable_firewall_for_keyvaults_and_storage" {
                                                        type        = bool
                                                      }
 
-variable "encryption_at_host_enabled"           {
-                                                  description = "Enables host encryption for sap vms"
-                                                  default     = false
-                                                  type        = bool
-                                                }                                                     
-
 variable "data_plane_available"                 {
                                                   description = "Boolean value indicating if storage account access is via data plane"
                                                   default     = false
