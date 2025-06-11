5efb48f19ec282c1864370baff8baa8c4a19ad37
Kimmo Forss <kimforss@microsoft.com>
2025-06-11 21:18:41 +0300

    Refactor management VNet variable assignments for clarity and consistency

--- deploy/terraform/run/sap_deployer/variables_local.tf
+++ deploy/terraform/run/sap_deployer/variables_local.tf
@@ -10,21 +10,21 @@ locals {
   version_label                        = trimspace(file("${path.module}/../../../configs/version.txt"))
 
   // Management vnet
+  vnet_mgmt_arm_id                     = try(local.infrastructure.vnets.management.arm_id, "")
+  vnet_mgmt_exists                     = length(local.vnet_mgmt_arm_id) > 0
 
   //There is no default as the name is mandatory unless arm_id is specified
-  vnet_mgmt_name                       = local.infrastructure.virtual_network.management.exists ? (
-                                           split("/", local.infrastructure.virtual_network.management.id)[8]) : (
-                                           length(local.infrastructure.virtual_network.management.name) > 0 ? (
-                                             local.infrastructure.virtual_network.management.name) : (
+  vnet_mgmt_name                       = local.vnet_mgmt_exists ? (
+                                           split("/", local.vnet_mgmt_arm_id)[8]) : (
+                                           length(local.infrastructure.vnets.management.name) > 0 ? (
+                                             local.infrastructure.vnets.management.name) : (
                                              "DEP00"
                                            )
                                          )
 
-  parsed_id                           = provider::azurerm::parse_resource_id(var.tfstate_resource_id)
+  saplib_subscription_id               = split("/", var.tfstate_resource_id)[2]
+  saplib_resource_group_name           = split("/", var.tfstate_resource_id)[4]
 
-  saplib_subscription_id              = local.parsed_id["subscription_id"]
-  saplib_resource_group_name           = local.parsed_id["resource_group_name"]
-  tfstate_storage_account_name        = local.parsed_id["resource_name"]
 
   // Default naming of vnet has multiple parts. Taking the second-last part as the name incase the name ends with -vnet
   vnet_mgmt_parts                      = length(split("-", local.vnet_mgmt_name))
@@ -41,7 +41,7 @@ locals {
   spn                                  = {
                                            subscription_id = coalesce(var.subscription_id, try(data.azurerm_key_vault_secret.subscription_id[0].value,null))
                                            client_id       = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value : null,
-                                           client_secret   = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value : null,
+                                           client_secret   = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value : null,
                                            tenant_id       = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value : null
                                          }
 
