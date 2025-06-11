464487502229eca37e748f18ce1d852a6f2d5be5
Kimmo Forss <kimforss@microsoft.com>
2025-06-11 20:36:03 +0300

    Add register_storage_accounts_keyvaults_with_dns variable to manage DNS registration for storage accounts and key vaults

--- deploy/terraform/run/sap_deployer/transform.tf
+++ deploy/terraform/run/sap_deployer/transform.tf
@@ -238,14 +238,15 @@ locals {
                                          }
 
   dns_settings                         = {
-                                           use_custom_dns_a_registration = var.use_custom_dns_a_registration
-                                           dns_zone_names = var.dns_zone_names
+                                           use_custom_dns_a_registration                = var.use_custom_dns_a_registration
+                                           dns_zone_names                               = var.dns_zone_names
 
-                                           management_dns_resourcegroup_name = trimspace(var.management_dns_resourcegroup_name)
-                                           management_dns_subscription_id = var.management_dns_subscription_id
+                                           management_dns_resourcegroup_name            = trimspace(var.management_dns_resourcegroup_name)
+                                           management_dns_subscription_id               = trimspace(var.management_dns_subscription_id)
 
-                                           privatelink_dns_subscription_id = var.privatelink_dns_subscription_id != var.management_dns_subscription_id ? var.privatelink_dns_subscription_id : var.management_dns_subscription_id
-                                           privatelink_dns_resourcegroup_name = var.management_dns_resourcegroup_name != var.privatelink_dns_resourcegroup_name ? var.privatelink_dns_resourcegroup_name : var.management_dns_resourcegroup_name
+                                           privatelink_dns_subscription_id              = trimspace(coalesce(var.privatelink_dns_subscription_id,var.management_dns_subscription_id, " "))
+                                           privatelink_dns_resourcegroup_name           = trimspace(coalesce(var.management_dns_resourcegroup_name, var.privatelink_dns_resourcegroup_name, " "))
+                                           register_storage_accounts_keyvaults_with_dns = var.register_storage_accounts_keyvaults_with_dns
                                          }
 
 }
