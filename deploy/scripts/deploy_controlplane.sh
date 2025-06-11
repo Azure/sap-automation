5c2f7ad0d7ff189fb5b34eb6eee62fef1643ff9d
Kimmo Forss <kimforss@microsoft.com>
2025-06-11 15:32:03 +0300

    Refactor relative path assignment in deploy_controlplane.sh for clarity

--- deploy/scripts/deploy_controlplane.sh
+++ deploy/scripts/deploy_controlplane.sh
@@ -579,7 +579,7 @@ if [ 2 -eq $step ]; then
 
 	relative_path="${library_dirname}"
 	export TF_DATA_DIR="${relative_path}/.terraform"
-	relative_path="$CONFIG_REPO_PATH/${deployer_dirname}"
+	relative_path="${deployer_dirname}"
 
 
 	cd "${library_dirname}" || exit
