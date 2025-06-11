55c01b594d0d1a0d3c62c0728c7d4a3bf6616f23
Kimmo Forss <kimforss@microsoft.com>
2025-06-11 21:07:00 +0300

    Update Key Vault secret references to use deployer_kv_user_arm_id variable

--- deploy/terraform/run/sap_deployer/imports.tf
+++ deploy/terraform/run/sap_deployer/imports.tf
@@ -10,23 +10,23 @@
 data "azurerm_key_vault_secret" "subscription_id" {
                                                     count        = var.use_spn ? 1 : 0
                                                     name         = format("%s-subscription-id", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
-                                                    key_vault_id = local.key_vault.id
+                                                    key_vault_id = var.deployer_kv_user_arm_id
                                                   }
 
 data "azurerm_key_vault_secret" "client_id"       {
                                                     count        = var.use_spn ? 1 : 0
                                                     name         = format("%s-client-id", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
-                                                    key_vault_id = local.key_vault.id
+                                                    key_vault_id = var.deployer_kv_user_arm_id
                                                   }
 
 ephemeral "azurerm_key_vault_secret" "client_secret"   {
                                                     count        = var.use_spn ? 1 : 0
                                                     name         = format("%s-client-secret", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
-                                                    key_vault_id = local.key_vault.id
+                                                    key_vault_id = var.deployer_kv_user_arm_id
                                                   }
 
 data "azurerm_key_vault_secret" "tenant_id"       {
                                                     count        = var.use_spn ? 1 : 0
                                                     name         = format("%s-tenant-id", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
-                                                    key_vault_id = local.key_vault.id
+                                                    key_vault_id = var.deployer_kv_user_arm_id
                                                   }
