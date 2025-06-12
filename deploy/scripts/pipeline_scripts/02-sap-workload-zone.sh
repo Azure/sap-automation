19e0fd15c1962f58ed23a9a6446503f043f7a528
Kimmo Forss <kimforss@microsoft.com>
2025-06-12 13:27:20 +0300

    Refactor logon variable handling to streamline ARM subscription ID management

--- deploy/scripts/pipeline_scripts/02-sap-workload-zone.sh
+++ deploy/scripts/pipeline_scripts/02-sap-workload-zone.sh
@@ -110,8 +110,6 @@ if [ $USE_MSI == "true" ]; then
 	ARM_USE_MSI=true
 	export ARM_USE_MSI
 fi
-ARM_SUBSCRIPTION_ID=$CP_ARM_SUBSCRIPTION_ID
-export ARM_SUBSCRIPTION_ID
 # Check if running on deployer
 if [[ ! -f /etc/profile.d/deploy_server.sh ]]; then
 	configureNonDeployer "$TF_VERSION"
@@ -343,15 +341,7 @@ echo -e "$green--- Deploy the workload zone ---$reset"
 cd "$CONFIG_REPO_PATH/LANDSCAPE/$WORKLOAD_ZONE_FOLDERNAME" || exit
 
 # Set logon variables
-if [ $USE_MSI != "true" ]; then
-	# Set logon variables
-	ARM_CLIENT_ID="$WL_ARM_CLIENT_ID"
-	export ARM_CLIENT_ID
-	ARM_CLIENT_SECRET="$WL_ARM_CLIENT_SECRET"
-	export ARM_CLIENT_SECRET
-	ARM_TENANT_ID=$WL_ARM_TENANT_ID
-	export ARM_TENANT_ID
-else
+if [ $USE_MSI == "true" ]; then
 	unset ARM_CLIENT_SECRET
 	ARM_USE_MSI=true
 	export ARM_USE_MSI
