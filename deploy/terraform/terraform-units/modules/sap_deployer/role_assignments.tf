fc91d7988c08a3002b376b3bf59cc9bfcad56051
Kimmo Forss <kimforss@microsoft.com>
2025-06-15 13:56:39 +0300

    Update role assignment conditions for User Access Administrator in sap_deployer module

--- deploy/terraform/terraform-units/modules/sap_deployer/role_assignments.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/role_assignments.tf
@@ -45,60 +45,60 @@ resource "azurerm_role_assignment" "resource_group_user_access_admin_msi" {
   scope                                = local.resource_group_exists ? data.azurerm_resource_group.deployer[0].id : azurerm_resource_group.deployer[0].id
   role_definition_name                 = "User Access Administrator"
   principal_id                         = length(var.deployer.user_assigned_identity_id) == 0 ? azurerm_user_assigned_identity.deployer[0].principal_id : data.azurerm_user_assigned_identity.deployer[0].principal_id
-  # condition_version                    = "2.0"
-  # condition                            = <<-EOT
-  #                                           (
-  #                                            (
-  #                                             !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
-  #                                            )
-  #                                            OR
-  #                                            (
-  #                                             @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
-  #                                            )
-  #                                           )
-  #                                           AND
-  #                                           (
-  #                                            (
-  #                                             !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
-  #                                            )
-  #                                            OR
-  #                                            (
-  #                                             @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
-  #                                            )
-  #                                           )
-  #                                           EOT
+  condition_version                    = "2.0"
+  condition                            = <<-EOT
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
+                                             )
+                                             OR
+                                             (
+                                              @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
+                                             )
+                                            )
+                                            AND
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
+                                             )
+                                             OR
+                                             (
+                                              @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
+                                             )
+                                            )
+                                            EOT
 }
 
 
 resource "azurerm_role_assignment" "resource_group_user_access_admin_spn" {
   provider                             = azurerm.main
-  count                                = var.assign_subscription_permissions ? 0 : 0
+  count                                = var.assign_subscription_permissions && length(var.spn_id) > 0 ? 1 : 0
   scope                                = local.resource_group_exists ? data.azurerm_resource_group.deployer[0].id : azurerm_resource_group.deployer[0].id
   role_definition_name                 = "User Access Administrator"
   principal_type                       = "ServicePrincipal"
   principal_id                         = var.spn_id
-  # condition_version                    = "2.0"
-  # condition                            = <<-EOT
-  #                                           (
-  #                                            (
-  #                                             !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
-  #                                            )
-  #                                            OR
-  #                                            (
-  #                                             @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
-  #                                            )
-  #                                           )
-  #                                           AND
-  #                                           (
-  #                                            (
-  #                                             !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
-  #                                            )
-  #                                            OR
-  #                                            (
-  #                                             @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
-  #                                            )
-  #                                           )
-  #                                           EOT
+  condition_version                    = "2.0"
+  condition                            = <<-EOT
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
+                                             )
+                                             OR
+                                             (
+                                              @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
+                                             )
+                                            )
+                                            AND
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
+                                             )
+                                             OR
+                                             (
+                                              @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
+                                             )
+                                            )
+                                            EOT
 }
 
 resource "azurerm_role_assignment" "role_assignment_msi" {
