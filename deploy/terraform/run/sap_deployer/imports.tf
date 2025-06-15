7774e624a3a9b0611c9c49de7c0aff9714e189d1
Kimmo Forss <kimforss@microsoft.com>
2025-06-15 17:43:41 +0300

    Update key vault secret names to use environment variable for consistency

--- deploy/terraform/run/sap_deployer/imports.tf
+++ deploy/terraform/run/sap_deployer/imports.tf
@@ -9,24 +9,24 @@
 
 data "azurerm_key_vault_secret" "subscription_id" {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-subscription-id", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
+                                                    name         = format("%s-subscription-id", upper(var.environment))
                                                     key_vault_id = var.deployer_kv_user_arm_id
                                                   }
 
 data "azurerm_key_vault_secret" "client_id"       {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-client-id", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
+                                                    name         = format("%s-client-id", upper(var.environment))
                                                     key_vault_id = var.deployer_kv_user_arm_id
                                                   }
 
 ephemeral "azurerm_key_vault_secret" "client_secret"   {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-client-secret", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
+                                                    name         = format("%s-client-secret", upper(var.environment))
                                                     key_vault_id = var.deployer_kv_user_arm_id
                                                   }
 
 data "azurerm_key_vault_secret" "tenant_id"       {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-tenant-id", upper(module.sap_namegenerator.naming.prefix.DEPLOYER))
+                                                    name         = format("%s-tenant-id", upper(var.environment))
                                                     key_vault_id = var.deployer_kv_user_arm_id
                                                   }
