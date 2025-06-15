4fc0e2c205bac5b870d92ed8dda49d6cf916848f
Kimmo Forss <kimforss@microsoft.com>
2025-06-15 17:56:44 +0300

    Refactor variable names for resource group and subscription ID for consistency in imports.tf

--- deploy/terraform/run/sap_deployer/variables_local.tf
+++ deploy/terraform/run/sap_deployer/variables_local.tf
@@ -22,9 +22,13 @@ locals {
                                            )
                                          )
 
-  saplib_subscription_id               = split("/", var.tfstate_resource_id)[2]
-  saplib_resource_group_name           = split("/", var.tfstate_resource_id)[4]
-  saplib_storage_account_name          = split("/", var.tfstate_resource_id)[6]
+ // Locate the tfstate storage account
+  parsed_id                           = provider::azurerm::parse_resource_id(var.tfstate_resource_id)
+
+  SAPLibrary_subscription_id          = local.parsed_id["subscription_id"]
+  SAPLibrary_resource_group_name      = local.parsed_id["resource_group_name"]
+  tfstate_storage_account_name        = local.parsed_id["resource_name"]
+  tfstate_container_name              = module.sap_namegenerator.naming.resource_suffixes.tfstate
 
 
   // Default naming of vnet has multiple parts. Taking the second-last part as the name incase the name ends with -vnet
