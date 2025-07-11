32c18fe27140df5ce756edc14049a8a716d3b1ff
Kimmo Forss <kimforss@microsoft.com>
2025-07-12 01:13:19 +0300

    fix: Update additional_network_id assignment to handle potential null values from remote state

--- deploy/terraform/bootstrap/sap_library/transform.tf
+++ deploy/terraform/bootstrap/sap_library/transform.tf
@@ -89,9 +89,9 @@ locals {
                                            create_privatelink_dns_zones              = var.create_privatelink_dns_zones
 
                                            additional_network_id                     = var.use_deployer ? (
-                                                                                         data.terraform_remote_state.deployer[0].outputs.additional_network_id != var.additional_network_id) ? (
+                                                                                         try(data.terraform_remote_state.deployer[0].outputs.additional_network_id, "") != var.additional_network_id) ? (
                                                                                           var.additional_network_id) : (
-                                                                                          data.terraform_remote_state.deployer[0].outputs.additional_network_id) : (
+                                                                                          try(data.terraform_remote_state.deployer[0].outputs.additional_network_id, "")) : (
                                                                                         var.additional_network_id)
                                          }
 
