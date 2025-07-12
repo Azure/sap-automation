f4aa9f68d123312328a2debb02214f0a5b0c21bb
Rickmarges <rickmarges@gmail.com>
2025-07-12 13:29:39 +0200

    2 HANA Scale out fixes for RHEL and Terraform fixes (#778)
    * Add extra check for ANF on DB loadbalancer
    
    * Make sudo rule more lenient
    
    * Provide Terraform fixes
    
    These fixes include:
    - Handle empty deployer state when not using a dpeloyer
    - Adding tags to more resources
    - Adding 'var.' before variables in output file
    
    ---------
    
    Co-authored-by: Marges, RSY (Rick) <rick.marges@achmea.nl>
    Co-authored-by: Hemanth Damecharla <71097261+hdamecharla@users.noreply.github.com>

--- deploy/terraform/terraform-units/modules/sap_system/output_files/inventory.tf
+++ deploy/terraform/terraform-units/modules/sap_system/output_files/inventory.tf
@@ -265,11 +265,10 @@ resource "local_file" "sap-parameters_yml" {
               enable_sap_cal              = var.enable_sap_cal
               calapi_kv                   = var.calapi_kv
               sap_cal_product_name        = var.sap_cal_product_name
-
-              single_server                          = length(var.webdispatcher_server_ips) + length(var.application_server_ips) + length(var.scs_server_ips) + length(var.database_server_ips) == 1 ? (
-                                                       true) : (
-                                                       false
-                                                     )
+              single_server               = length(var.webdispatcher_server_ips) + length(var.application_server_ips) + length(var.scs_server_ips) + length(var.database_server_ips) == 1 ? (
+                                            true) : (
+                                            false
+                                          )
 
     }
   )
