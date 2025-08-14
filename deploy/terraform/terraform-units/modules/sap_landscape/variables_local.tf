da47b81ae46f273ae1664362c40e4b90d140c77e
Kimmo Forss <kimforss@microsoft.com>
2025-08-14 19:16:18 +0300

    Enhance Terraform configurations by updating dependencies in virtual network peering resources and refining firewall existence check logic.

--- deploy/terraform/terraform-units/modules/sap_landscape/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/variables_local.tf
@@ -23,7 +23,7 @@ locals {
   region                                          = var.infrastructure.region
 
   // Firewall
-  firewall_exists                                 = var.use_deployer ? length(trimspace(coalesce(var.deployer_tfstate.firewall_id, ""))) > 0 : false
+  firewall_exists                                 = var.use_deployer ? length(contains(keys(var.deployer_tfstate),"firewall_id") ? var.deployer_tfstate.firewall_id : "") > 0 : false
   firewall_id                                     = local.firewall_exists ? try(var.deployer_tfstate.firewall_id, "") : ""
   firewall_ip                                     = local.firewall_exists ? try(var.deployer_tfstate.firewall_ip, "") : ""
   firewall_name                                   = local.firewall_exists ? try(split("/", local.firewall_id)[8], "") : ""
