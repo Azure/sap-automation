083f4ed6cca63f7558e0a19bf45fcb0bdcd12349
Kimmo Forss <kimforss@microsoft.com>
2025-08-14 14:28:19 +0300

    fix: add dependencies for additional network peering resources

--- deploy/terraform/terraform-units/modules/sap_landscape/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/infrastructure.tf
@@ -184,6 +184,7 @@ resource "azurerm_virtual_network_peering" "peering_sap_management" {
 # // Peers additional VNET to SAP VNET
 resource "azurerm_virtual_network_peering" "peering_additional_network_sap" {
   provider                             = azurerm.peering
+  depends_on                           = [ azurerm_subnet.admin, azurerm_subnet.app, azurerm_subnet.db, azurerm_subnet.web, azurerm_subnet.iscsi, azurerm_subnet.storage, azurerm_subnet.ams, azurerm_subnet.anf ]
   count                                = length(try(var.infrastructure.additional_network_id, "")) > 0 ? 1 : 0
   name                                 = substr(
                                            format("%s_to_%s",
@@ -210,6 +211,7 @@ resource "azurerm_virtual_network_peering" "peering_additional_network_sap" {
 
 resource "azurerm_virtual_network_peering" "peering_sap_additional_network" {
   provider                             = azurerm.main
+  depends_on                           = [ azurerm_subnet.admin, azurerm_subnet.app, azurerm_subnet.db, azurerm_subnet.web, azurerm_subnet.iscsi, azurerm_subnet.storage, azurerm_subnet.ams, azurerm_subnet.anf ]
   count                                = length(try(var.infrastructure.additional_network_id, "")) > 0 ? 1 : 0
   name                                 = substr(
                                            format("%s_to_%s",
