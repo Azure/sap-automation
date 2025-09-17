c79006a432673047c8ab9bac6ebb949893887a8e
Kimmo Forss <kimforss@microsoft.com>
2025-09-17 17:46:53 +0200

    fix: update tfstate handling by renaming deployer_tf_state_key to deployer_tfstate_key and streamline variable loading

--- deploy/scripts/install_workloadzone.sh
+++ deploy/scripts/install_workloadzone.sh
@@ -296,9 +296,9 @@ if [ -z "${REMOTE_STATE_SA}" ]; then
 	load_config_vars "${deployer_environment_file_name}" "REMOTE_STATE_SA"
 fi
 load_config_vars "${deployer_environment_file_name}" "tfstate_resource_id"
-load_config_vars "${deployer_environment_file_name}" "deployer_tf_state_key"
-deployer_tf_state_key="${deployerTerraformStatefileName}"
-save_config_var "deployer_tf_state_key" "${workload_environment_file_name}"
+load_config_vars "${deployer_environment_file_name}" ""
+deployer_tfstate_key="${deployerTerraformStatefileName}"
+save_config_var "deployer_tfstate_key" "${workload_environment_file_name}"
 
 if [ -n "$REMOTE_STATE_SA" ]; then
 	getAndStoreTerraformStateStorageAccountDetails "${REMOTE_STATE_SA}" "${workload_environment_file_name}"
