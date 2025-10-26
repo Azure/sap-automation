d7030e8bc9eb50d1ed22efaf2c831eb20387d47e
Kimmo Forss <kimforss@microsoft.com>
2025-10-27 01:15:03 +0200

    fix: improve role assignment logging for Managed Identity in New-SDAFADOWorkloadZone function

--- deploy/scripts/pwsh/SDAFUtilities/Public/New-SDAFADOWorkloadZone.ps1
+++ deploy/scripts/pwsh/SDAFUtilities/Public/New-SDAFADOWorkloadZone.ps1
@@ -413,7 +413,8 @@ function New-SDAFADOWorkloadZone {
         foreach ($RoleName in $Roles) {
 
           Write-Host "Assigning role" $RoleName "to the Managed Identity" -ForegroundColor Green
-          $roleAssignment = az role assignment create --assignee-object-id $ManagedIdentityObjectId --assignee-principal-type ServicePrincipal --role $RoleName --scope /subscriptions/$WorkloadZoneSubscriptionId --query id --output tsv --only-show-errors
+          Write-Verbose "Assigning role" $RoleName "to the Managed Identity ($ManagedIdentityObjectId)" -ForegroundColor Green
+          $roleAssignment = az role assignment create --assignee-object-id $ManagedIdentityObjectId --role $RoleName --scope /subscriptions/$WorkloadZoneSubscriptionId --query id --output tsv --only-show-errors
           if ($roleAssignment) {
             Write-Host "Successfully assigned $RoleName role to identity" -ForegroundColor Green
             Write-Verbose "Role assignment ID: $roleAssignment"
