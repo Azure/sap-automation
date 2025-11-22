4578cb9d9c57dc6bdb538cd736f4e727c3ef33b1
Kimmo Forss <kimforss@microsoft.com>
2025-11-22 02:45:04 +0200

    Refactor key vault secret name formatting to use local.environment_name for consistent naming

--- deploy/terraform/run/sap_deployer/imports.tf
+++ deploy/terraform/run/sap_deployer/imports.tf
@@ -7,26 +7,34 @@
 #                                                                              #
 #######################################4#######################################8
 
+locals {
+  # Determine effective naming based on configuration
+  use_control_plane_naming                     = length(trimspace(var.control_plane_name)) > 0
+  environment_name                             = local.use_control_plane_naming ? var.control_plane_name : var.environment
+
+
+}
+
 data "azurerm_key_vault_secret" "subscription_id" {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-subscription-id", upper(var.environment))
+                                                    name         = format("%s-subscription-id", upper(local.environment_name))
                                                     key_vault_id = var.spn_keyvault_id
                                                   }
 
 data "azurerm_key_vault_secret" "client_id"       {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-client-id", upper(var.environment))
+                                                    name         = format("%s-client-id", upper(local.environment_name))
                                                     key_vault_id = var.spn_keyvault_id
                                                   }
 
 ephemeral "azurerm_key_vault_secret" "client_secret"   {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-client-secret", upper(var.environment))
+                                                    name         = format("%s-client-secret", upper(local.environment_name))
                                                     key_vault_id = var.spn_keyvault_id
                                                   }
 
 data "azurerm_key_vault_secret" "tenant_id"       {
                                                     count        = var.use_spn ? 1 : 0
-                                                    name         = format("%s-tenant-id", upper(var.environment))
+                                                    name         = format("%s-tenant-id", upper(local.environment_name))
                                                     key_vault_id = var.spn_keyvault_id
                                                   }
