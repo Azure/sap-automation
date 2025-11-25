157961a826426fc0bf360fcdc44fd570bdbddead
Nadeen Noaman <95418928+nnoaman@users.noreply.github.com>
2025-11-25 14:36:21 +0100

    Fix: Update ansible-playbook command to use the correct private key path

--- deploy/scripts/pipeline_scripts/v1/05-run-ansible.sh
+++ deploy/scripts/pipeline_scripts/v1/05-run-ansible.sh
@@ -176,12 +176,12 @@ echo "Check if file: ${filename} exists"
 if [ -f "${filename}" ]; then
 	echo "##[group]- preconfiguration"
 
-	redacted_command="ansible-playbook -i '$INVENTORY' --private-key $SAP_PARAMS/$SSH_KEY_NAME -e 'kv_name=$VAULT_NAME' -e 'download_directory=$AGENT_TEMPDIRECTORY' -e '_workspace_directory=$curdir' $EXTRA_PARAMS -e orchestration_ansible_user=$USER -e ansible_user=$user_name -e ansible_python_interpreter=/usr/bin/python3 -e @$SAP_PARAMS $EXTRA_PARAM_FILE	${filename}"
+	redacted_command="ansible-playbook -i '$INVENTORY' --private-key $curdir/$SSH_KEY_NAME -e 'kv_name=$VAULT_NAME' -e 'download_directory=$AGENT_TEMPDIRECTORY' -e '_workspace_directory=$curdir' $EXTRA_PARAMS -e orchestration_ansible_user=$USER -e ansible_user=$user_name -e ansible_python_interpreter=/usr/bin/python3 -e @$SAP_PARAMS $EXTRA_PARAM_FILE	${filename}"
 
 	echo "##[section]Executing [$redacted_command]..."
 
 	command="ansible-playbook -i '$INVENTORY'                                    \\
-	              --private-key $SAP_PARAMS/$SSH_KEY_NAME                        \\
+	              --private-key $curdir/$SSH_KEY_NAME                            \\
 								-e 'kv_name=$VAULT_NAME'                                       \\
 								-e 'download_directory=$AGENT_TEMPDIRECTORY'                   \\
 								-e '_workspace_directory=$curdir' $EXTRA_PARAMS                \\
@@ -198,8 +198,8 @@ if [ -f "${filename}" ]; then
 
 fi
 
-command="ansible-playbook -i '$INVENTORY'                                      \
-	              --private-key $SAP_PARAMS/$SSH_KEY_NAME                        \
+command="ansible-playbook -i '$INVENTORY'                                      \\
+	              --private-key $curdir/$SSH_KEY_NAME                            \\
 								-e 'kv_name=$VAULT_NAME'                                       \
 								-e 'download_directory=$AGENT_TEMPDIRECTORY'                   \
 								-e '_workspace_directory=$curdir' $EXTRA_PARAMS                \
