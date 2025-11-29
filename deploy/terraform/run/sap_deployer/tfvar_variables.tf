d650565d96513536db0b289fbe8c04751138e715
Kimmo Forss <kimforss@microsoft.com>
2025-11-29 16:41:02 +0200

    Fix: Remove default value for 'environment' variable to enforce specification

--- deploy/terraform/run/sap_deployer/tfvar_variables.tf
+++ deploy/terraform/run/sap_deployer/tfvar_variables.tf
@@ -11,7 +11,6 @@
 variable "environment"                           {
                                                    description = "This is the environment name of the deployer"
                                                    type        = string
-                                                   default     = ""
                                                    validation {
                                                      condition     = length(var.environment) <= 5 && length(var.environment) > 0
                                                      error_message = "The 'environment' variable must be specified and at most 5 characters long."
