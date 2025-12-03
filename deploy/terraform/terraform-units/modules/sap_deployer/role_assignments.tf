51f1c9c1b6258cd23d9a5fc3ff8ad83af1a23f33
Kimmo Forss <kimforss@microsoft.com>
2025-12-03 21:25:02 +0200

    Update role_assignment for user access admin to use Role Based Access Control Administrator and refine conditions

--- deploy/terraform/terraform-units/modules/sap_deployer/role_assignments.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/role_assignments.tf
@@ -141,31 +141,31 @@ resource "azurerm_role_assignment" "resource_group_user_access_admin_spn" {
   provider                             = azurerm.main
   count                                = var.options.assign_resource_permissions && !local.run_as_msi ? 1 : 0
   scope                                = var.infrastructure.resource_group.exists ? data.azurerm_resource_group.deployer[0].id : azurerm_resource_group.deployer[0].id
-  role_definition_name                 = "User Access Administrator"
+  role_definition_name                 = "Role Based Access Control Administrator"
   principal_type                       = "ServicePrincipal"
   principal_id                         = data.azurerm_client_config.current.object_id
-   # condition_version                    = "2.0"
-  # condition                            = <<-EOT
-  #                                           (
-  #                                            (
-  #                                             !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
-  #                                            )
-  #                                            OR
-  #                                            (
-  #                                             @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
-  #                                            )
-  #                                           )
-  #                                           AND
-  #                                           (
-  #                                            (
-  #                                             !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
-  #                                            )
-  #                                            OR
-  #                                            (
-  #                                             @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
-  #                                            )
-  #                                           )
-  #                                           EOT
+  condition_version                    = "2.0"
+  condition                            = <<-EOT
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
+                                             )
+                                             OR
+                                             (
+                                              @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {ba92f5b4-2d11-453d-a403-e96b0029c9fe, 4633458b-17de-408a-b874-0445c86b69e6}
+                                             )
+                                            )
+                                            AND
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
+                                             )
+                                             OR
+                                             (
+                                              @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {ba92f5b4-2d11-453d-a403-e96b0029c9fe}
+                                             )
+                                            )
+                                            EOT
 }
 
 resource "azurerm_role_assignment" "role_assignment_spn" {
