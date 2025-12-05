ed08629a739aaa8a09c022b13a7dc67a3305a952
Kimmo Forss <kimforss@microsoft.com>
2025-12-05 19:24:56 +0200

    Update azurerm provider version to 4.55.0 across multiple Terraform configurations

--- deploy/terraform/run/sap_deployer/providers.tf
+++ deploy/terraform/run/sap_deployer/providers.tf
@@ -123,7 +123,7 @@ terraform                              {
                                                                          }
                                                               azurerm =  {
                                                                            source  = "hashicorp/azurerm"
-                                                                           version = "4.54.0"
+                                                                           version = "4.55.0"
                                                                          }
                                                               azapi =   {
                                                                            source  = "Azure/azapi"
