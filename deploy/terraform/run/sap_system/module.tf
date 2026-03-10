7ed4238691f920dc6ff035b3f7ccd039d7c1812d
Kimmo Forss <kimforss@microsoft.com>
2026-03-10 11:51:28 +0200

    Add support for network security perimeter for SDU
    Update network security perimeter association to use hanashared IDs

--- deploy/terraform/run/sap_system/module.tf
+++ deploy/terraform/run/sap_system/module.tf
@@ -137,6 +137,7 @@ module "hdb_node" {
   db_asg_id                                     = module.common_infrastructure.db_asg_id
   db_subnet                                     = module.common_infrastructure.db_subnet
   deploy_application_security_groups            = var.deploy_application_security_groups
+  deployer_tfstate                              = try(data.terraform_remote_state.deployer[0].outputs, [])
   deployment                                    = var.deployment
   dns_settings                                  = local.dns_settings
   enable_firewall_for_keyvaults_and_storage     = var.enable_firewall_for_keyvaults_and_storage
---
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/network_security_perimeter.tf
@@ -0,0 +1,54 @@
+
+data "azurerm_network_security_perimeter" "perimeter" {
+  provider                             = azurerm.deployer
+  count                                = try(var.deployer_tfstate.network_security_perimeter_deployment, false) ? 1 : 0
+  name                                 = local.network_security_name
+  resource_group_name                  = local.network_security_resource_group_name
+}
+
+
+data "azurerm_network_security_perimeter_profile" "profile" {
+  provider                             = azurerm.deployer
+  count                                = try(var.deployer_tfstate.network_security_perimeter_deployment, false) ? 1 : 0
+  name                                 = "SDAF"
+  network_security_perimeter_id        = data.azurerm_network_security_perimeter.perimeter[0].id
+}
+
+resource "azurerm_network_security_perimeter_association" "sapmnt" {
+  provider                               = azurerm.deployer
+  count                                  = try(var.deployer_tfstate.network_security_perimeter_deployment, false) ? 1 : 0
+  name                                   = length(var.azure_files_sapmnt_id) > 0 ? (
+                                                     data.azurerm_storage_account.sapmnt[0].name) : (
+                                                     try(azurerm_storage_account.sapmnt[0].name, "")
+                                                   )
+  access_mode                            = var.deployer_tfstate.network_security_access_mode
+
+  network_security_perimeter_profile_id = data.azurerm_network_security_perimeter_profile.profile[0].id
+  resource_id                           = length(var.azure_files_sapmnt_id) > 0 ? (
+                                                     data.azurerm_storage_account.sapmnt[0].id) : (
+                                                     try(azurerm_storage_account.sapmnt[0].id, "")
+                                                   )
+}
+
+resource "azurerm_network_security_perimeter_association" "sid_keyvault_user" {
+  provider                               = azurerm.deployer
+  count                                  = try(var.deployer_tfstate.network_security_perimeter_deployment, false) ? 1 : 0
+  name                                   = local.enable_sid_deployment && local.use_local_credentials && length(local.user_key_vault_id) == 0 ? (
+                                                     data.azurerm_key_vault.sid_keyvault_user[0].name) : (
+                                                     azurerm_key_vault.sid_keyvault_user[0].name
+                                                   )
+  access_mode                            = var.deployer_tfstate.network_security_access_mode
+
+  network_security_perimeter_profile_id = data.azurerm_network_security_perimeter_profile.profile[0].id
+  resource_id                           = local.enable_sid_deployment && local.use_local_credentials && length(local.user_key_vault_id) == 0 ? (
+                                                     data.azurerm_key_vault.sid_keyvault_user[0].id) : (
+                                                     azurerm_key_vault.sid_keyvault_user[0].id
+                                                   )
+}
+
+locals {
+  parsed_network_security_id           = try(var.deployer_tfstate.network_security_perimeter_deployment, false) ? try(provider::azurerm::parse_resource_id(var.deployer_tfstate.network_security_perimeter_id), "") : null
+  network_security_name                = try(var.deployer_tfstate.network_security_perimeter_deployment, false) ? local.parsed_network_security_id["resource_name"] : ""
+  network_security_resource_group_name = try(var.deployer_tfstate.network_security_perimeter_deployment, false) ? local.parsed_network_security_id["resource_group_name"] : ""
+
+}
