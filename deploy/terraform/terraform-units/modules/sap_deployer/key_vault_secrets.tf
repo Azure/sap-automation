eefb2c539fd597a959f1c8e248bc731587bbbaf8
Kimmo Forss <kimmo.forss@microsoft.com>
2026-03-26 13:18:46 +0000

    Remove unused PAT and web_pwd key vault secret resources from key_vault_secrets.tf

--- deploy/terraform/terraform-units/modules/sap_deployer/key_vault_secrets.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/key_vault_secrets.tf
@@ -88,57 +88,6 @@ resource "azurerm_key_vault_secret" "username" {
   }
 }
 
-resource "azurerm_key_vault_secret" "pat" {
-  count                                = local.enable_key && (length(var.infrastructure.devops.agent_pat)> 0 ) && !var.key_vault.exists  ? 0 : 0
-  depends_on                           = [
-                                           time_sleep.wait_for_keyvault
-                                         ]
-  name                                 = "PAT"
-  value                                = var.infrastructure.devops.agent_pat
-  key_vault_id                         = var.key_vault.exists ? (
-                                           var.key_vault.id) : (
-                                           azurerm_key_vault.kv_user[0].id
-                                         )
-
-  expiration_date                      = var.set_secret_expiry ? (
-                                           time_offset.secret_expiry_date.rfc3339) : (
-                                           null
-                                         )
-  content_type                         = "secret"
-  tags                                 = var.infrastructure.tags
-  lifecycle {
-    ignore_changes = [ expiration_date]
-  }
-}
-
-# resource "azurerm_key_vault_secret" "web_pwd" {
-#   count                                = (local.enable_key && !local.key_exist) ? (
-#                                           (
-#                                             !var.bootstrap || !var.key_vault.exists) ? (
-#                                             1) : (
-#                                             0
-#                                           )) : (
-#                                           0
-#                                         )
-#   depends_on                           = [
-#                                            azurerm_key_vault_access_policy.kv_user_pre_deployer[0],
-#                                            azurerm_key_vault_access_policy.kv_user_msi,
-#                                            azurerm_key_vault_access_policy.kv_user_systemidentity,
-#                                          ]
-
-#   name                                 = "WEB-PWD"
-#   value                                = var.webapp_client_secret
-#   key_vault_id                         = var.key_vault.exists ? (
-#                                            var.key_vault.id) : (
-#                                            azurerm_key_vault.kv_user[0].id
-#                                          )
-
-#   expiration_date                      = var.set_secret_expiry ? (
-#                                            time_offset.secret_expiry_date.rfc3339) : (
-#                                            null
-#                                          )
-# }
-
 resource "azurerm_key_vault_secret" "pwd" {
   count                                = local.enable_password && (length(var.key_vault.username_secret_name) == 0 ) && !var.key_vault.exists  ? 1 : 0
   depends_on                           = [
