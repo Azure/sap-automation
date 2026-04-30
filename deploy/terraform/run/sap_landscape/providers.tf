6c11951d95539fc649393ccf8dd3616058af8bfd
Kimmo Forss <kimforss@microsoft.com>
2026-04-30 15:36:44 +0300

    Update azurerm provider version to 4.70.0 across multiple Terraform configurations

--- deploy/terraform/run/sap_landscape/providers.tf
+++ deploy/terraform/run/sap_landscape/providers.tf
@@ -129,7 +129,7 @@ terraform                              {
                                                                          }
                                                               azurerm =  {
                                                                            source  = "hashicorp/azurerm"
-                                                                           version = "4.63.0"
+                                                                           version = "4.70.0"
                                                                          }
                                                               azapi =    {
                                                                            source  = "azure/azapi"
--- deploy/terraform/run/sap_library/providers.tf
+++ deploy/terraform/run/sap_library/providers.tf
@@ -121,7 +121,7 @@ terraform                              {
                                                                          }
                                                               azurerm =  {
                                                                            source  = "hashicorp/azurerm"
-                                                                           version = "4.63.0"
+                                                                           version = "4.70.0"
                                                                          }
                                                             }
                                        }
