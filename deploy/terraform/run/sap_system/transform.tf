08bb9dd3f897864a2d16220478c51a533d43077b
Devansh Jain (from Dev Box) <devanshjain@microsoft.com>
2026-07-21 16:52:50 -0700

    Enhance SAP deployment scripts and configurations

--- deploy/terraform/run/sap_system/transform.tf
+++ deploy/terraform/run/sap_system/transform.tf
@@ -99,7 +99,7 @@ locals {
                                            database_server_count           = var.database_high_availability ? 2 * var.database_server_count : var.database_server_count
                                            database_hana_use_saphanasr_angi =  upper(var.database_platform) == "HANA" ? (
                                                                                  var.database_high_availability ? (
-                                                                                     var.use_saphanasr_angi || var.use_sles_saphanasr_angi
+                                                                                     startswith(try(var.database_vm_image.offer, ""), "sles-sap-16") ? true : var.use_sles_saphanasr_angi
                                                                                      ) : (
                                                                                        false
                                                                                      )
@@ -108,7 +108,7 @@ locals {
                                                                                  )
 
                                            database_vm_sku                 = var.database_vm_sku
-                                           db_sizing_key                   = coalesce(var.db_sizing_dictionary_key, var.database_size, "Default")
+                                           db_sizing_key                   = coalesce(var.db_sizing_dictionary_key, var.database_size, "Optimized")
                                            deploy_v1_monitoring_extension  = var.deploy_v1_monitoring_extension
                                            disk_controller_type_database_tier   = var.disk_controller_type_database_tier
                                            dual_network_interfaces         = var.database_dual_nics
@@ -142,6 +142,15 @@ locals {
                                            scale_out                       = var.database_HANA_use_scaleout_scenario
                                            stand_by_node_count             = var.stand_by_node_count
                                            zones                           = var.database_vm_zones
+                                           database_hana_use_saphanasr_angi =  upper(var.database_platform) == "HANA" ? (
+                                                                                 var.database_high_availability ? (
+                                                                                     startswith(try(var.database_vm_image.offer, ""), "sles-sap-16") ? true : var.use_sles_saphanasr_angi
+                                                                                     ) : (
+                                                                                       false
+                                                                                     )
+                                                                                 ) : (
+                                                                                   false
+                                                                                 )
 
                                            disk_controller_type_database_tier   = var.disk_controller_type_database_tier
                                          }
@@ -334,16 +343,19 @@ locals {
 
   scs_os_specified                  = (length(local.scs_os.source_image_id) + length(local.scs_os.publisher)) > 0
 
-  validated_use_simple_mount        = var.use_simple_mount ? (
-                                        upper(local.scs_os.publisher) != "SUSE" || !(var.scs_high_availability) ? (
-                                         false) : (
-                                         contains(["sles-sap-15-sp3", "sles-sap-15-sp4", "sles-sap-15-sp5", "sles-sap-15-sp6"], local.scs_os.offer) ? (
-                                           var.use_simple_mount) : (
-                                           false
-                                         )
-                                       )) : (
-                                       false
-                                       )
+  validated_use_simple_mount        = (
+                                        upper(local.scs_os.publisher) == "SUSE" && var.scs_high_availability
+                                      ) ? (
+                                        startswith(local.scs_os.offer, "sles-sap-16") ? (
+                                          true) : (
+                                          var.use_simple_mount && contains(["sles-sap-15-sp3", "sles-sap-15-sp4", "sles-sap-15-sp5", "sles-sap-15-sp6", "sles-sap-15-sp7"], local.scs_os.offer) ? (
+                                            true) : (
+                                            false
+                                          )
+                                        )
+                                      ) : (
+                                        false
+                                      )
 
   web_os                            = {
                                         os_type         = coalesce(try(var.webdispatcher_server_image.os_type, null), try(var.application_server_image.os_type, null), "LINUX")
