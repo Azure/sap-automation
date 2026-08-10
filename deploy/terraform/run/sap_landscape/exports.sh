Azure/sap-automation

commit 3ab403efb7456b543d9f7b905b21c67e7973aafe

Implement utility storage account versioning and immutability policies; update related tests and documentation

deploy/terraform/run/sap_landscape/exports.sh
@@ -1,0 +1,11 @@
+#!/bin/bash
+export                 ARM_SUBSCRIPTION_ID="00000000-0000-0000-0000-000000000000"
+
+unset SDAF_APPLICATION_CONFIGURATION_NAME
+
+export SDAF_TERRAFORM_STORAGE_ACCOUNT_NAME="tfstatesa"
+unset SDAF_WORKLOAD_ZONE_KEYVAULT_NAME
+# This is the name of the key vault to be used for this workload zone. 
+export                  SDAF_WORKLOAD_ZONE_KEYVAULT_NAME="kv-deployer"
+export             SDAF_CONTROL_PLANE_NAME="DEV-WEEU-DEP00"
+export             SDAF_WORKLOAD_ZONE_NAME="DEV-WEEU-SAP01"

deploy/terraform/run/sap_landscape/imports.tf
@@ -68,7 +68,7 @@ data "azurerm_key_vault_secret" "client_id" {
   }
 }
 
-ephemeral "azurerm_key_vault_secret" "client_secret" {
+data "azurerm_key_vault_secret" "client_secret" {
   count                                = var.use_spn ? 1 : 0
   name                                 = format("%s-client-secret", local.environment_name)
   key_vault_id                         = local.key_vault.spn.id
@@ -109,7 +109,7 @@ data "azurerm_key_vault_secret" "cp_client_id" {
   }
 }
 
-ephemeral "azurerm_key_vault_secret" "cp_client_secret" {
+data "azurerm_key_vault_secret" "cp_client_secret" {
   count                                = local.retrieve_cp_credentials ? 1 : 0
   name                                 = format("%s-client-secret", local.control_plane_name_resolved)
   key_vault_id                         = local.key_vault.spn.id

deploy/terraform/run/sap_landscape/providers.tf
@@ -40,7 +40,7 @@ provider "azurerm"                     {
                                                   }
                                          subscription_id            = length(var.subscription_id) > 0 ? var.subscription_id : data.azurerm_key_vault_secret.subscription_id[0].value
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value: null
-                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value: null
+                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value: null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value: null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -55,7 +55,7 @@ provider "azurerm"                     {
                                          alias                      = "deployer"
                                          subscription_id            = coalesce(var.management_dns_subscription_id, local.tfstate_storage_account_subscription_id, length(local.deployer_subscription_id) > 0 ? local.deployer_subscription_id : "", local.tfstate_storage_account_subscription_id)
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null
-                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.cp_client_secret[0].value : null
+                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -67,7 +67,7 @@ provider "azurerm"                     {
                                          alias                      = "dnsmanagement"
                                          subscription_id            = coalesce(var.management_dns_subscription_id, local.tfstate_storage_account_subscription_id, length(local.deployer_subscription_id) > 0 ? local.deployer_subscription_id : "")
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null
-                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.cp_client_secret[0].value : null
+                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -78,7 +78,7 @@ provider "azurerm"                     {
                                          alias                      = "privatelinkdnsmanagement"
                                          subscription_id            = coalesce(var.privatelink_dns_subscription_id, var.management_dns_subscription_id, local.tfstate_storage_account_subscription_id, length(local.deployer_subscription_id) > 0 ? local.deployer_subscription_id : "")
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null
-                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.cp_client_secret[0].value : null
+                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -94,7 +94,7 @@ provider "azurerm"                     {
 
 provider "azuread"                     {
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value : null
-                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value : null
+                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                        }
@@ -103,7 +103,7 @@ provider "azapi"                       {
                                          alias                      = "api"
                                          subscription_id            = length(var.subscription_id) > 0 ? var.subscription_id : data.azurerm_key_vault_secret.subscription_id[0].value
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value : null
-                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value : null
+                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                       }

commit 63753715ca217c299e99d2d2f1f7d3a1fe4334fc

fix: restore ephemeral key vault secrets, drop stray exports.sh and verbose descriptions
Commit 3ab403efb accidentally captured a CI-only test workaround that
rewrites ephemeral azurerm_key_vault_secret to data, which persists the
SPN client secret in plaintext Terraform state. Restore the ephemeral
resources in imports.tf and providers.tf, remove the stray exports.sh
scratch file, and revert the utility_storage_accounts description text
to its original wording.

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>
Copilot-Session: 73fed5da-b713-4992-90ef-0ba4a11473e6

deploy/terraform/run/sap_landscape/exports.sh
@@ -1,11 +1,0 @@
-#!/bin/bash
-export                 ARM_SUBSCRIPTION_ID="00000000-0000-0000-0000-000000000000"
-
-unset SDAF_APPLICATION_CONFIGURATION_NAME
-
-export SDAF_TERRAFORM_STORAGE_ACCOUNT_NAME="tfstatesa"
-unset SDAF_WORKLOAD_ZONE_KEYVAULT_NAME
-# This is the name of the key vault to be used for this workload zone. 
-export                  SDAF_WORKLOAD_ZONE_KEYVAULT_NAME="kv-deployer"
-export             SDAF_CONTROL_PLANE_NAME="DEV-WEEU-DEP00"
-export             SDAF_WORKLOAD_ZONE_NAME="DEV-WEEU-SAP01"

deploy/terraform/run/sap_landscape/imports.tf
@@ -68,7 +68,7 @@ data "azurerm_key_vault_secret" "client_id" {
   }
 }
 
-data "azurerm_key_vault_secret" "client_secret" {
+ephemeral "azurerm_key_vault_secret" "client_secret" {
   count                                = var.use_spn ? 1 : 0
   name                                 = format("%s-client-secret", local.environment_name)
   key_vault_id                         = local.key_vault.spn.id
@@ -109,7 +109,7 @@ data "azurerm_key_vault_secret" "cp_client_id" {
   }
 }
 
-data "azurerm_key_vault_secret" "cp_client_secret" {
+ephemeral "azurerm_key_vault_secret" "cp_client_secret" {
   count                                = local.retrieve_cp_credentials ? 1 : 0
   name                                 = format("%s-client-secret", local.control_plane_name_resolved)
   key_vault_id                         = local.key_vault.spn.id

deploy/terraform/run/sap_landscape/providers.tf
@@ -40,7 +40,7 @@ provider "azurerm"                     {
                                                   }
                                          subscription_id            = length(var.subscription_id) > 0 ? var.subscription_id : data.azurerm_key_vault_secret.subscription_id[0].value
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value: null
-                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value: null
+                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value: null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value: null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -55,7 +55,7 @@ provider "azurerm"                     {
                                          alias                      = "deployer"
                                          subscription_id            = coalesce(var.management_dns_subscription_id, local.tfstate_storage_account_subscription_id, length(local.deployer_subscription_id) > 0 ? local.deployer_subscription_id : "", local.tfstate_storage_account_subscription_id)
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null
-                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null
+                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.cp_client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -67,7 +67,7 @@ provider "azurerm"                     {
                                          alias                      = "dnsmanagement"
                                          subscription_id            = coalesce(var.management_dns_subscription_id, local.tfstate_storage_account_subscription_id, length(local.deployer_subscription_id) > 0 ? local.deployer_subscription_id : "")
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null
-                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null
+                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.cp_client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -78,7 +78,7 @@ provider "azurerm"                     {
                                          alias                      = "privatelinkdnsmanagement"
                                          subscription_id            = coalesce(var.privatelink_dns_subscription_id, var.management_dns_subscription_id, local.tfstate_storage_account_subscription_id, length(local.deployer_subscription_id) > 0 ? local.deployer_subscription_id : "")
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null
-                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null
+                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.cp_client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                          storage_use_azuread        = true
@@ -94,7 +94,7 @@ provider "azurerm"                     {
 
 provider "azuread"                     {
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value : null
-                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value : null
+                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                        }
@@ -103,7 +103,7 @@ provider "azapi"                       {
                                          alias                      = "api"
                                          subscription_id            = length(var.subscription_id) > 0 ? var.subscription_id : data.azurerm_key_vault_secret.subscription_id[0].value
                                          client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value : null
-                                         client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value : null
+                                         client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value : null
                                          tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value : null
                                          use_msi                    = var.use_spn ? false : true
                                       }

deploy/terraform/run/sap_landscape/tfvar_variables.tf
@@ -1123,7 +1123,7 @@ variable "utility_vm_zones"                        {
 #########################################################################################
 
 variable "utility_storage_accounts"                {
-                                                     description = "List of utility storage account configurations for the workload zone. Container immutability policies are optional. A locked policy cannot be unlocked, shortened or deleted: Azure never permits deleting a locked time-based retention policy, and Terraform must destroy the policy before its container and storage account. Locking therefore blocks the normal terraform destroy path for the container, the storage account and the workload zone permanently; waiting for retention to expire does not unblock it. Teardown then requires an out-of-band procedure (empty and delete the container through the control plane once retention has expired, then remove the policy from state). Account-level version_level_immutability is separate from container immutability and requires versioning_enabled: Azure only accepts it when the storage account is created, so adding it to an account that already exists replaces that account and destroys its data. Set it only on new utility storage accounts. It is also mutually exclusive with point-in-time restore."
+                                                     description = "List of utility storage account configurations for the workload zone. Container immutability policies are optional. A locked policy cannot be unlocked, shortened or deleted: Azure never permits deleting a locked time-based retention policy, and Terraform must destroy the policy before its container and storage account. Locking therefore blocks the normal terraform destroy path for the container, the storage account and the workload zone permanently; waiting for retention to expire does not unblock it. Teardown then requires an out-of-band procedure (empty and delete the container through the control plane once retention has expired, then remove the policy from state)."
                                                      type = list(object({
                                                        name                     = optional(string, "")
                                                        account_kind             = optional(string, "FileStorage")

deploy/terraform/terraform-units/modules/sap_landscape/variables_global.tf
@@ -268,7 +268,7 @@ variable "vm_settings"                                   {
                                                          }
 
 variable "utility_storage_settings"                      {
-                                                           description = "List of utility storage account configurations normalized from transform.tf. Container immutability policies are optional. A locked policy cannot be unlocked, shortened or deleted: Azure never permits deleting a locked time-based retention policy, and Terraform must destroy the policy before its container and storage account. Locking therefore blocks the normal terraform destroy path for the container, the storage account and the workload zone permanently; waiting for retention to expire does not unblock it. Teardown then requires an out-of-band procedure (empty and delete the container through the control plane once retention has expired, then remove the policy from state). Account-level version_level_immutability is separate from container immutability and requires versioning_enabled: Azure only accepts it when the storage account is created, so adding it to an account that already exists replaces that account and destroys its data. Set it only on new utility storage accounts. It is also mutually exclusive with point-in-time restore."
+                                                           description = "List of utility storage account configurations normalized from transform.tf. Container immutability policies are optional. A locked policy cannot be unlocked, shortened or deleted: Azure never permits deleting a locked time-based retention policy, and Terraform must destroy the policy before its container and storage account. Locking therefore blocks the normal terraform destroy path for the container, the storage account and the workload zone permanently; waiting for retention to expire does not unblock it. Teardown then requires an out-of-band procedure (empty and delete the container through the control plane once retention has expired, then remove the policy from state)."
                                                            type = list(object({
                                                              name                       = string
                                                              account_kind               = string