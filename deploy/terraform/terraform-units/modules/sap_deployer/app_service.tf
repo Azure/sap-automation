79abd3ec6d0baa60073f5f68216865692f5756d5
Kimmo Forss <kimforss@microsoft.com>
2026-09-17 15:54:27 +0300

    Add SourceBranch variable to app service configuration

--- deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
@@ -140,6 +140,7 @@ resource "azurerm_windows_web_app" "webapp" {
     "CONTROLPLANE_LOC"                        = var.naming_new.location_short
     "CONTROL_PLANE_NAME"                      = upper(format("%s-%s-%s", var.infrastructure.environment, var.naming_new.location_short, var.infrastructure.virtual_network.logical_name))
     "TFSTATE_STORAGE_ACCOUNT_NAME"            = format("https://%s.blob.core.windows.net", try(var.app_service.tfstate_storage_account_name, ""))
+    "SourceBranch"                            = var.infrastructure.devops.branch
   }
 
   sticky_settings {
